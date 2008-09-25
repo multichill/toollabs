1761ed841b04dd5bad3909159ea2c91588e8ba24
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2008-09-25 21:52:07 +0000

    Bot to get a list of categories which should be subcategories of some country
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@5 7e777f60-21ef-46b6-b16d-5948f447dc97

--- bot/nocountry.py
+++ bot/nocountry.py
@@ -8,12 +8,12 @@ sys.path.append("/home/multichill/pywikipedia")
 import wikipedia, MySQLdb, config
 
 def connectDatabase():
-    conn = MySQLdb.connect(config.db_hostname, db='commonswiki_p', user = config.db_username, passwd = config.db_password)
+    conn = MySQLdb.connect(config.db_hostname, db='u_multichill', user = config.db_username, passwd = config.db_password)
     cursor = conn.cursor()
     return (conn, cursor)
 
 def getCountryList(cursor):
-    query = u"SELECT pl_title FROM page JOIN pagelinks ON page_id=pl_from WHERE page_namespace=2 AND page_is_redirect=0 AND page_title = 'Multichill/Countries' AND pl_namespace=14"
+    query = u"SELECT pl_title FROM commonswiki_p.page JOIN commonswiki_p.pagelinks ON page_id=pl_from WHERE page_namespace=2 AND page_is_redirect=0 AND page_title = 'Multichill/Countries' AND pl_namespace=14"
     cursor.execute(query)
     result = []
     while True:
@@ -25,66 +25,28 @@ def getCountryList(cursor):
 	    break
     return result
 
-def getByCountryList(cursor, startSubject):
-    query = u"SELECT cat.page_title, bc.page_title FROM page AS bc JOIN categorylinks ON bc.page_id = cl_from JOIN page AS cat ON (cl_to=cat.page_title AND bc.page_title=CONCAT(cat.page_title, '_by_country')) WHERE bc.page_namespace=14 AND bc.page_is_redirect=0 AND bc.page_title LIKE '%by_country' AND cat.page_namespace=14 AND cat.page_is_redirect=0"
-    cursor.execute(query)
+def getNoCountry(cursor, country):
+    cursor.execute(u"SELECT c1.child FROM cats AS c1 WHERE c1.child LIKE %s AND NOT EXISTS(SELECT * FROM cats AS c2 JOIN cats AS c3 ON (c2.parent=c3.child) JOIN cats AS c4 ON (c3.parent=c4.child) WHERE c1.child=c2.child AND (c2.parent LIKE %s OR c3.parent LIKE %s OR c4.parent LIKE %s OR c2.parent='Category_redirects')) AND NOT c1.child=%s", ('%' + country, '%' + country + '%', '%' + country + '%', '%' + country + '%', country))
     result = []
     while True:
 	try:
-	    subject, subjectByCountry = cursor.fetchone()
-	    #print subject + ' ' + subjectByCountry
-	    result.append((unicode(subject, 'utf-8'), unicode(subjectByCountry, 'utf-8')))
-	except TypeError:
-	    # Limit reached or no more results
-	    break
-    return result
-
-def getMissingByCountry(cursor, subject, subjectByCountry, countries):
-    cursor.execute(u"SELECT page_title FROM page WHERE page_namespace=14 AND page_is_redirect=0 AND page_title LIKE %s AND NOT EXISTS(SELECT * FROM categorylinks WHERE cl_from=page_id AND cl_to = %s) AND NOT EXISTS(SELECT * FROM templatelinks WHERE page_id=tl_from AND tl_title ='Category_redirect')", (subject + '_%' , subjectByCountry))
-    result = []
-    while True:
-	try:
-	    country = None
 	    cat, = cursor.fetchone()
 	    cat = unicode(cat, 'utf-8')
-	    #print "bla"
-	    country = isCountryCategory(cat, subject, countries)
-	    if country:
-		result.append((cat, subjectByCountry, country))
-		#print cat + ' should be in ' + subjectByCountry
-	    #print cat
+	    #print cat + country
+	    result.append((cat, country))
 	except TypeError:
 	    # Limit reached or no more results
 	    break
-    #print "bla!" + result
     return result
-
-def isCountryCategory(cat, subject, countries):
-    for country in countries:
-	if cat.endswith(country):
-	    if (cat == subject + u'_from_' + country) or (cat == subject + u'_from_the_' + country):
-		return country
-	    elif (cat == subject + u'_in_' + country) or (cat == subject + u'_in_the_' + country):
-		return country
-	    elif (cat == subject + u'_of_' + country) or (cat == subject + u'_of_the_' + country):
-		return country
-    return None
 
 def outputResult(missingCatsTotal):
     resultwiki = u''
-    resultscript = u'#!/usr/pkg/bin/bash\n'
-    page = wikipedia.Page(wikipedia.getSite(u'commons', u'commons'), u'User:Multichill/By_country_to_fix')
-    comment = u'A list of categories to fix'
-    for (cat, subjectByCountry, country) in missingCatsTotal:
-	resultwiki = resultwiki +  u'*[[:Category:' + cat + u']] should be in [[:Category:' + subjectByCountry + u']]\n'
-	resultscript = resultscript + u'python2.4 add_text.py -always -lang:commons -family:commons -page:"Category:' + cat + u'" -text:"[[Category:' + subjectByCountry.replace(u'_', u' ') + u'|' + country.replace(u'_', u' ') + u']]" -summary:"Adding [[Category:' + subjectByCountry.replace(u'_', u' ') + u']]"\n' 
+    page = wikipedia.Page(wikipedia.getSite(u'commons', u'commons'), u'User:Multichill/No_country')
+    comment = u'A list categories which should be subcategories of some country'
+    for (cat, country) in missingCatsTotal:
+	resultwiki = resultwiki +  u'*[[:Category:' + cat + u']] should be a subcategory of [[:Category:' + country + u']]\n'
     resultwiki = resultwiki.replace(u'_', u' ')
-    #resultscript = resultscript.replace(u'_', u' ')
     page.put(resultwiki, comment)
-    #f = file("/home/multichill/queries/bycountry.txt", 'w')
-    #f.write(resultscript.encode('utf-8'))
-    #f.close()
-    #wikipedia.output(resultscript)
 
 def main():
     conn = None
@@ -93,14 +55,13 @@ def main():
     (conn, cursor) = connectDatabase()
     countries = getCountryList(cursor)
     
-    byCountryList = getByCountryList(cursor, u'')
 
-    for (subject, subjectByCountry) in byCountryList:
-	missingCats = getMissingByCountry(cursor, subject, subjectByCountry, countries)
-	if missingCats:
-	    missingCatsTotal = missingCatsTotal + missingCats
-    #for (a, b) in missingCatsTotal:
-    #	print a + ' should be in ' + b
+    for country in countries:
+    	missingCats = getNoCountry(cursor, country)
+    	if missingCats:
+    	    missingCatsTotal = missingCatsTotal + missingCats
+    for (a, b) in missingCatsTotal:
+    	print a + ' should be in ' + b
 
     outputResult(missingCatsTotal)
     
