3ea7ba88c6c6f18d40bd5e8634e7c5b86c47b0c2
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2008-10-07 19:45:13 +0000

    Dir for all the create scripts
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@10 7e777f60-21ef-46b6-b16d-5948f447dc97

---
+++ bot/create_page/media_needing_categories.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+'''
+Simple script to create the daily subcategory of http://commons.wikimedia.org/wiki/Category:Media_needing_categories
+The bot will create todays category and tomorrows category if they didnt exist yet.
+
+'''
+import sys
+sys.path.append("/home/multichill/pywikipedia/")
+import wikipedia
+from datetime import datetime
+from datetime import timedelta
+
+def createCategory (date = None):
+    day = date.strftime('%d')
+    month = date.strftime('%B')
+    year = date.strftime('%Y')
+    page = wikipedia.Page(wikipedia.getSite(u'commons', u'commons'), u'Category:Media needing categories as of ' + str(int(day)) + u' ' + month + u' ' + year)
+    wikipedia.output(u'Working on ' + page.title())
+    if not page.exists():
+	toPut = u'{{UncategorizedHeader|day=' + day + u'|month=' + month + u'|year=' + year + u'}}' 
+	wikipedia.output(u'Creating category with content: ' + toPut)
+	page.put(toPut, toPut)
+    else:
+	wikipedia.output(u'Category already exists')
+
+def main():
+    today = datetime.utcnow()
+    tomorrow = today + timedelta(days=+1)
+    createCategory(today)
+    createCategory(tomorrow)
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        wikipedia.stopme()
