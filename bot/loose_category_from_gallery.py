106e8b9376f13af4e99fd66db49a1f663c7e30c4
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2008-11-06 21:38:49 +0000

    Seems stable
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@27 7e777f60-21ef-46b6-b16d-5948f447dc97

--- bot/loose_category_from_gallery.py
+++ bot/loose_category_from_gallery.py
@@ -15,12 +15,12 @@ def connectDatabase():
     return (conn, cursor)
 
 
-def getImagesToCategorize(cursor):
+def getImagesToCategorize(cursor, uncat):
     '''
     Get all images with are in a subcategory of Media needing categories and which are in use in a gallery which has a category with the same name. The gallery should be in the category or the gallery and the category should share the same parent category'
 
     '''
-    query=u"SELECT ipage.page_title AS img, gpage.page_title AS gal, galcl.cl_to AS cat FROM page AS ipage JOIN categorylinks AS uncl ON ipage.page_id=uncl.cl_from JOIN imagelinks ON ipage.page_title=il_to JOIN page AS gpage ON il_from=gpage.page_id JOIN categorylinks AS galcl ON gpage.page_id=galcl.cl_from WHERE ipage.page_namespace=6 AND ipage.page_is_redirect=0 AND gpage.page_namespace=0 AND gpage.page_is_redirect=0 AND uncl.cl_to = 'Media_needing_categories_as_of_27_July_2008' LIMIT 20"
+    query=u"SELECT ipage.page_title AS img, gpage.page_title AS gal, galcl.cl_to AS cat FROM page AS ipage JOIN categorylinks AS uncl ON ipage.page_id=uncl.cl_from JOIN imagelinks ON ipage.page_title=il_to JOIN page AS gpage ON il_from=gpage.page_id JOIN categorylinks AS galcl ON gpage.page_id=galcl.cl_from WHERE ipage.page_namespace=6 AND ipage.page_is_redirect=0 AND gpage.page_namespace=0 AND gpage.page_is_redirect=0 AND uncl.cl_to = %s"
     
     result = []
     lastimage = ''
@@ -30,7 +30,7 @@ def getImagesToCategorize(cursor):
     gals = []
     cats = []
 
-    cursor.execute(query)
+    cursor.execute(query, (uncat.replace(' ', '_'),))
     while True:
 	try:
 	    image, gal, cat = cursor.fetchone()
@@ -44,7 +44,7 @@ def getImagesToCategorize(cursor):
                 cats.append((unicode(cat, 'utf-8')))
 	    else:
 		#Add the previous image and do some cleanup
-		result.append((lastimage, list(set(gals)), list(set(cats))))
+		result.append((unicode(lastimage, 'utf-8'), list(set(gals)), list(set(cats))))
 		lastimage= ''
 		gals = []
 		cats = []
@@ -54,7 +54,8 @@ def getImagesToCategorize(cursor):
                 cats.append((unicode(cat, 'utf-8')))
 	except TypeError:
 	    # Limit reached or no more results
-	    result.append((lastimage, list(set(gals)), list(set(cats))))
+	    if(lastimage!=''):
+		result.append((unicode(lastimage, 'utf-8'), list(set(gals)), list(set(cats))))
 	    break
     return result
 
@@ -64,14 +65,14 @@ def categorizeImage(image, gals, cats):
     '''
     page = wikipedia.Page(wikipedia.getSite(), u'Image:' + image)
     #category = category.replace('_', ' ')
-    if page.exists():
+    if (page.exists() and gals and cats):
 	newtext = imagerecat.removeTemplates(page.get())
 	newtext = newtext + imagerecat.getCheckCategoriesTemplate([], gals)
 	for category in cats:
 	    newtext = newtext + u'[[Category:' + category.replace('_', ' ') + u']]\n'
 	comment = u'Adding one or more categories (based on gallery information) to this uncategorized image'
 	wikipedia.showDiff(page.get(), newtext)
-	#page.put(newtext, comment)
+	page.put(newtext, comment)
 
 def main():
     '''
@@ -83,9 +84,20 @@ def main():
     (conn, cursor) = connectDatabase()
 
     imagerecat.initLists()
-
-    for (image, gals, cats) in getImagesToCategorize(cursor):
-	categorizeImage(image, gals, imagerecat.applyAllFilters(cats))
+    generator = None
+    for arg in wikipedia.handleArgs():
+        if arg.startswith('-page'):
+            if len(arg) == 5:
+                generator = [wikipedia.Page(wikipedia.getSite(), wikipedia.input(u'What page do you want to use?'))]
+            else:
+                generator = [wikipedia.Page(wikipedia.getSite(), arg[6:])]
+        else:
+            generator = genFactory.handleArg(arg)
+    if generator:
+        for page in generator:
+	    if((page.namespace() == 14) and (page.title().startswith(u'Category:Media needing categories as of'))):
+		for (image, gals, cats) in getImagesToCategorize(cursor, page.titleWithoutNamespace()):
+		    categorizeImage(image, gals, imagerecat.applyAllFilters(cats))
 	
 
     
