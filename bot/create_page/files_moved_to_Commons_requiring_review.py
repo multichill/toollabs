4443c18cb408463575cf6a17e30b4324525f3577
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2009-05-07 15:07:49 +0000

    Create daily project pages too
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@57 7e777f60-21ef-46b6-b16d-5948f447dc97

--- bot/create_page/files_moved_to_Commons_requiring_review.py
+++ bot/create_page/files_moved_to_Commons_requiring_review.py
@@ -11,7 +11,9 @@ import wikipedia
 from datetime import datetime
 from datetime import timedelta
 
-def createCategory (date = None):
+projects = [(u'en', u'wikipedia'), (u'nl', 'wikipedia')]
+
+def createCategory (date = datetime.utcnow()):
     day = date.strftime('%d')
     month = date.strftime('%B')
     year = date.strftime('%Y')
@@ -24,11 +26,30 @@ def createCategory (date = None):
     else:
 	wikipedia.output(u'Category already exists')
 
+def createProjectCategory (date = datetime.utcnow(), lang=u'en', project=u'wikipedia'):
+    day = date.strftime('%d')
+    month = date.strftime('%B')
+    year = date.strftime('%Y')
+    page = wikipedia.Page(wikipedia.getSite(u'commons', u'commons'), u'Category:Files moved from ' + lang + u'.' + project + u' to Commons requiring review as of ' + str(int(day)) + u' ' + month + u' ' + year)
+    wikipedia.output(u'Working on ' + page.title())
+    if not page.exists():
+        toPut = u'{{BotMoveToCommonsHeader|lang=' + lang + u'|project=' + project + u'|day=' + day + u'|month=' + month + u'|year=' + year + u'}}'
+        wikipedia.output(u'Creating category with content: ' + toPut)
+        page.put(toPut, toPut)
+    else:
+        wikipedia.output(u'Category already exists')
+
 def main():
     today = datetime.utcnow()
     tomorrow = today + timedelta(days=+1)
+
     createCategory(today)
+    for (lang, project) in projects:
+	createProjectCategory(today, lang, project)
+
     createCategory(tomorrow)
+    for (lang, project) in projects:
+        createProjectCategory(tomorrow, lang, project)
 
 if __name__ == "__main__":
     try:
