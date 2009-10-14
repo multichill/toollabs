8e61e12afca1de9a0354f1c0fc3f731d01edf81a
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2009-10-14 16:53:06 +0000

    Bot to upload all fema images
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@81 7e777f60-21ef-46b6-b16d-5948f447dc97

---
+++ bot/fema/fema_bot.py
@@ -0,0 +1,201 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+import sys, os, StringIO, hashlib, base64
+import os.path
+import urllib, re
+from datetime import datetime
+from BeautifulSoup import BeautifulSoup 
+sys.path.append("/home/multichill/pywikipedia")
+import wikipedia, upload
+import config
+
+def downloadPhoto(photoUrl = ''):
+    '''
+    Download the photo and store it in a StrinIO.StringIO object.
+
+    TODO: Add exception handling
+    '''
+    imageFile=urllib.urlopen(photoUrl).read()
+    return StringIO.StringIO(imageFile)
+
+def findDuplicateImages(photo = None, site = wikipedia.getSite(u'commons', u'commons')):
+    '''
+    Takes the photo, calculates the SHA1 hash and asks the mediawiki api for a list of duplicates.
+
+    TODO: Add exception handling, fix site thing
+    '''
+    hashObject = hashlib.sha1()
+    hashObject.update(photo.getvalue())
+    return site.getFilesFromAnHash(base64.b16encode(hashObject.digest()))
+
+def getDate(datestring):
+    '''
+    Try to get a date from the string or return the string
+    '''
+
+    #MM/DD/YYYY
+    usformat = u'%m/%d/%Y'
+    isoformat = '%Y-%m-%d'
+    date = datetime.strptime(datestring, usformat)
+    return date.strftime(isoformat)
+
+def getMetadata(photo_id):
+    #caption = u''
+    photoinfo = {}
+    photoinfo['Categories:'] = []
+     
+    location = u''
+    photographer = u''
+    date = u''
+    #id is already known
+    #size is not needed
+    #dimensions is not needed
+    disasters = [] # One or more?
+    #categories = []
+    original=u'' # http://www.fema.gov/photodata/original/<photo_id>.jpg ?
+    
+    params = {'id' : photo_id }
+    tosend = urllib.urlencode(params)
+    femaPage = urllib.urlopen("http://www.photolibrary.fema.gov/photolibrary/photo_details.do", tosend)
+    data = femaPage.read()
+    soup = BeautifulSoup(data)
+    if soup.find('div', {'class' : 'caption'}):
+        #print soup.find('div', {'class' : 'caption'}).string.strip()
+        photoinfo['caption'] = soup.find('div', {'class' : 'caption'}).string.strip()
+        photoinfo['title'] = soup.find('h2').string.strip()
+
+        photoinfo2 = soup.find('div', {'class' : 'photoinfo2'}).find('table', {'summary' : 'table used for layout purposes only'})
+        for row in photoinfo2.findAll("tr"):
+            ths = row.findAll("th")
+            tds = row.findAll("td")
+            #print ths
+            #print tds
+            if ths[0] and tds[0]:
+                if ths[0].string and tds[0].string:
+                    photoinfo[ths[0].string] = tds[0].string
+                    #print ths[0].string + ' - ' +  tds[0].string
+       
+                    if ths[1].string and tds[1].string:
+                        photoinfo[ths[1].string] = tds[1].string
+                        #print ths[1].string + ' - ' +  tds[1].string
+                #Disasters or Categories
+                else:
+                    if row.th.string == u'Disasters:':
+                        photoinfo[row.th.string] = row.td.a.string
+                        #print row.th.string
+                        #print row.td.a.string
+                    if row.th.string == u'Categories:':
+                        #print row.th.string
+                        for item in row.td.contents:
+                            if item.string:
+                                if item.string.strip():                                    
+                                    photoinfo['Categories:'].append(item.string.strip())
+
+        return photoinfo
+    else:
+        return False
+
+def buildDescription(photo_id, metadata):
+    description = u''
+
+    description = description + u'== {{int:filedesc}} ==\n'
+    description = description + u'{{Information\n'
+    description = description + u'|description={{en|1=' + metadata.get('caption') + u'}}\n'
+    description = description + u'|date=' + getDate(metadata.get('Photo Date:')) + u'\n' # MM/DD/YYYY
+    description = description + u'|source={{FEMA Photo Library|' + str(photo_id) + u'}}\n'
+    description = description + u'|author=' + metadata.get('Photographer:') + u'\n'
+    description = description + u'|permission=\n'
+    description = description + u'|other_versions=\n'
+    description = description + u'|other_fields=\n'
+    description = description + u'}}\n'
+    description = description + u'\n'
+    description = description + u'== {{int:license}} ==\n'
+    description = description + u'{{PD-USGov-FEMA}}\n'
+    description = description + u'\n'
+    if metadata.get('Disasters:'):    
+        description = description + u'[[Category:Images from FEMA, disaster ' + metadata.get('Disasters:') + u']]\n'
+    for cat in metadata.get('Categories:'):
+        description = description + u'[[Category:Images from FEMA, category ' + cat + u']]\n'
+    description = description + u''
+    description = description + u''
+    description = description + u''
+
+    return description
+
+def buildTitle(photo_id, metadata):
+    title = u'FEMA - ' + str(photo_id) + u' - ' + metadata['title'] + '.jpg'
+
+    title = re.sub(u"[<{\\[]", u"(", title)
+    title = re.sub(u"[>}\\]]", u")", title)
+    title = re.sub(u"[ _]?\\(!\\)", u"", title)
+    title = re.sub(u",:[ _]", u", ", title)
+    title = re.sub(u"[;:][ _]", u", ", title)
+    title = re.sub(u"[\t\n ]+", u" ", title)
+    title = re.sub(u"[\r\n ]+", u" ", title)
+    title = re.sub(u"[\n]+", u"", title)
+    title = re.sub(u"[?!]([.\"]|$)", u"\\1", title)
+    title = re.sub(u"[&#%?!]", u"^", title)
+    title = re.sub(u"[;]", u",", title)
+    title = re.sub(u"[/+\\\\:]", u"-", title)
+    title = re.sub(u"--+", u"-", title)
+    title = re.sub(u",,+", u",", title)
+    title = re.sub(u"[-,^]([.]|$)", u"\\1", title)
+    title = title.replace(u" ", u"_")   
+    
+    return title
+
+def processPhoto(photo_id):
+
+    # Get all the metadata
+    metadata = getMetadata(photo_id)
+    if not metadata:
+        #Incorrect photo_id
+        return
+
+    photoUrl = u'http://www.fema.gov/photodata/original/' + str(photo_id) + '.jpg'
+    photo = downloadPhoto(photoUrl)
+
+    duplicates = findDuplicateImages(photo)
+    # We don't want to upload tupes
+    if duplicates:
+        wikipedia.output(u'Found duplicate image at %s' % duplicates.pop())
+        return
+    
+    title = buildTitle(photo_id, metadata)
+    description = buildDescription(photo_id, metadata)
+
+    bot = upload.UploadRobot(photoUrl, description=description, useFilename=title, keepFilename=True, verifyDescription=False, targetSite = wikipedia.getSite('commons', 'commons'))
+    bot.upload_image(debug=False)
+
+def processPhotos(start_id=0, end_id=45000):
+    for i in range(start_id, end_id):
+        processPhoto(photo_id=i)
+        
+
+def main(args):
+    '''
+    Main loop.
+    '''
+    start_id = 0
+    end_id   = 45000
+    for arg in wikipedia.handleArgs():
+        if arg.startswith('-start_id'):
+            if len(arg) == 9:
+                start_id = wikipedia.input(u'What is the id of the photo you want to start at?')
+            else:
+                start_id = arg[10:]
+        elif arg.startswith('-end_id'):
+            if len(arg) == 7:
+                end_id = wikipedia.input(u'What is the id of the photo you want to end at?')
+            else:
+                end_id = arg[8:]
+                
+    processPhotos(int(start_id), int(end_id))
+  
+            
+         
+if __name__ == "__main__":
+    try:
+        main(sys.argv[1:])
+    finally:
+        print u'All done'
