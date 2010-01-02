b43f5ac353ca9c271fdcc2d8b0cb5573fd572195
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2010-01-02 18:11:52 +0000

    Create stament to build the database
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@109 7e777f60-21ef-46b6-b16d-5948f447dc97

---
+++ bot/erfgoed/create_table_monumenten.sql
@@ -0,0 +1,22 @@
+/* Create table statement for the monumenten database */
+connect u_multichill sql.toolserver.org;
+CREATE TABLE `monumenten` (
+  `objrijksnr` int(11) NOT NULL DEFAULT '0',
+  `woonplaats` varchar(255) NOT NULL DEFAULT '',
+  `adres` varchar(255) NOT NULL DEFAULT '',
+  `objectnaam` varchar(255) NOT NULL DEFAULT '',
+  `type_obj` ENUM('G', 'A'),
+  `oorspr_functie` varchar(128) NOT NULL DEFAULT '',
+  `bouwjaar` int(4),
+  `architect` varchar(255) NOT NULL DEFAULT '',
+  `cbs_tekst` varchar(255) NOT NULL DEFAULT '',
+  `RD_x` double NOT NULL DEFAULT '0',
+  `RD_y` double NOT NULL DEFAULT '0',
+  `lat` double NOT NULL DEFAULT '0',
+  `lon` double NOT NULL DEFAULT '0',
+  `source` varchar(255) NOT NULL DEFAULT '',
+  `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`objrijksnr`),
+  KEY `latitude` (`lat`),
+  KEY `longitude` (`lon`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
