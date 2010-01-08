33ba17454d8254df1f576259363415b7f5df8fde
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2010-01-08 00:50:38 +0000

    Flinke update * Wikilinks worden verwerkt * Aanpassing icoontjes * Betere namen * Mooiere layout * Foto van object met een link naar het bestand (als er een foto is) * Deep uplink link naar Commons
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@117 7e777f60-21ef-46b6-b16d-5948f447dc97

--- public_html/monumenten_op_de_kaart/getMonument.php
+++ public_html/monumenten_op_de_kaart/getMonument.php
@@ -1,7 +1,8 @@
 <?php 
+header('Content-type: application/vnd.google-earth.kml+xml');
 #header('Content-type: application/vnd.google-earth.kml+xml');
 
-function getIcon( $oorspr_functie) {
+function getIcon($type_obj, $oorspr_functie) {
     $size = 17;
     $icons = array(
         'Aanlegvoorziening' => 'Gfi-set01-hostel.svg',
@@ -70,17 +71,110 @@ function getIcon( $oorspr_functie) {
         'Winkel' => 'Gfi-set01-hostel.svg',
         'Woonhuis' => 'Gfi-set01-hostel.svg'
     );
-    if (array_key_exists($oorspr_functie, $icons)) {
-	return getImage($icons[$oorspr_functie], $size);
+    if ($type_obj=='A') {
+	return getImageFromCommons('Map_symbol_archaeology.svg', $size);
     } else {
-	return getImage('Gfi-set01-hostel.svg', $size);
+	if (array_key_exists($oorspr_functie, $icons)) {
+	    return getImageFromCommons($icons[$oorspr_functie], $size);
+	} else {
+	    return getImageFromCommons('Gfi-set01-hostel.svg', $size);
+	}
     }
 }
 
-function getImage($filename, $size) {
+function getImageFromCommons($filename, $size) {
     $md5hash=md5(filename);
     $url = "http://upload.wikimedia.org/wikipedia/commons/thumb/" . $md5hash[0] . "/" . $md5hash[0] . $md5hash[1] . "/" . urlencode($filename) . "/" . $size . "px-" . urlencode($filename);
     return $url; 
+}
+
+function getName($row) {
+    /* Build the title for the item */
+    $result = "";
+    if ($row['objectnaam']!="") {
+	$result = processWikitext($row['objectnaam'], 0);
+    } else {
+	$result = "Rijksmonument " .  $row['objrijksnr'];
+    }    
+    if ($row['woonplaats']!="") {
+	$result = $result . ", " . $row['woonplaats'];
+    }
+    return $result;
+}
+
+function processWikitext($text, $links) {
+    /* Process the wikitext.
+     * If links is true, make html links
+     * If links is false, remove wikitext to produce normal text without links
+     */
+    $result = $text;
+    $differentLinkRegex="/\[\[([^\|]*)\|([^\]]*)\]\]/";
+    $simpleLinkRegex="/\[\[([^\]]*)\\]\]/";
+    $differentLinkReplace = "'<a href=http://nl.wikipedia.org/wiki/' . rawurlencode('$1') . '>$2</a>'";
+    $simpleLinkReplace = "'<a href=http://nl.wikipedia.org/wiki/' . rawurlencode('$1') . '>$1</a>'";
+    if ( $links ) {
+	$result = preg_replace($differentLinkRegex . "e", $differentLinkReplace, $result);
+	$result = preg_replace($simpleLinkRegex . "e", $simpleLinkReplace, $result);
+	$result = $result;
+    } else {
+	$result = preg_replace($differentLinkRegex, "$2", $result);
+	$result = preg_replace($simpleLinkRegex, "$1", $result);
+    }
+    return $result;
+}
+
+function getImage($row) {
+    /* Return an image to be used or a request for an image */
+    if ($row['image'] != '') {
+	$result = '<a href="http://commons.wikimedia.org/wiki/File:' . rawurlencode($row['image']) . '">';
+	$result = $result . '<img src="' .  getImageFromCommons(str_replace(' ', '_', $row['image']), 200) . '" align="right" />';
+	$result = $result . '</a>';
+    } else {
+	$result = '<a href="' . makeUploadLink($row) .'" title="Upload een zelfgemaakte foto van dit monument naar Commons" target="_blank">';
+	$result = $result . '<img src="' .  getImageFromCommons('Crystal_Clear_app_lphoto.png', 100) . '" align="right" />';
+	$result = $result . '</a>';
+    }
+    return $result;
+}
+function makeUploadLink($row) {
+    /* Make an upload link with as much information as possible */
+    $result = 'http://commons.wikimedia.org/wiki/Commons:Upload';
+    $result = 'http://commons.wikimedia.org/w/index.php?title=Special:Upload&uploadformstyle=basic';
+    if ( $row['objectnaam']!='' ) {
+	if ( $row['woonplaats']!='' ) {
+	    $description = $row['objectnaam'] . ', ' . $row['woonplaats'] . ' (rijksmonument ' . $row['objrijksnr'] . ')';
+	} else {
+	    $description = $row['objectnaam'] . ' (rijksmonument ' . $row['objrijksnr'] . ')';
+	}
+    } else {
+	if ( $row['woonplaats']!='' ) {
+	    if ( $row['adres']!='' ) {
+		$description = 'Rijksmonument ' . $row['objrijksnr'] . ' (' . $row['adres'] . ' ' . $row['woonplaats'] . ')';
+	    } else {
+		$description = 'Rijksmonument ' . $row['objrijksnr'] . ' (' . $row['woonplaats'] . ')';	
+	    }
+	} else {
+	    $description = 'Rijksmonument ' . $row['objrijksnr'];
+	}
+    }
+    $wpDestFile = $description . '.jpg';
+    $wpUploadDescription = '{{Information \n';
+    $wpUploadDescription = $wpUploadDescription . '|Description={{nl|1=' . $description . '}}\n';
+    $wpUploadDescription = $wpUploadDescription . '|Source={{own}}\n';
+    $wpUploadDescription = $wpUploadDescription . '|Date=~~~~~ (upload date)\n';
+    $wpUploadDescription = $wpUploadDescription . '|Author=~~~\n';
+    $wpUploadDescription = $wpUploadDescription . '|Permission=\n';
+    $wpUploadDescription = $wpUploadDescription . '|other_versions=\n';
+    $wpUploadDescription = $wpUploadDescription . '}}\n';
+    $wpUploadDescription = $wpUploadDescription . '{{Object location dec|' . $row['lat'] . '|' . $row['lon'] . '}}\n';
+    $wpUploadDescription = $wpUploadDescription . '<!-- Information produced by http://toolserver.org/~multichill/monumenten_op_de_kaart/ from ' . $row['source'] . '-->\n';  
+    $wpUploadDescription = urlencode($wpUploadDescription);
+    $wpUploadDescription = str_replace('%5Cn', '%0A', $wpUploadDescription); 
+    $result = $result . '&wpDestFile=' . urlencode($wpDestFile);
+    $result = $result . '&wpUploadDescription=' . $wpUploadDescription;
+    //$wpDestFile = '';
+    //$wpUploadDescription = '';
+    return $result;
 }
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 
@@ -115,42 +209,46 @@ $row = mysql_fetch_assoc($result);
     <colorMode>random</colorMode>
     <scale>1.2</scale>
     <Icon>
-     <href><? echo getIcon( $row['oorspr_functie'] )?></href>
+     <href><? echo getIcon($row['type_obj'], $row['oorspr_functie'] )?></href>
      <? #<href>http://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Gfi-set01-hostel.svg/30px-Gfi-set01-hostel.svg.png</href> ?>
     </Icon>
    </IconStyle>
    </Style>
   <Placemark>
-   <name><? echo $row['objrijksnr'] ?>, <? echo $row['objectnaam'] ?> (<? echo $row['woonplaats'] ?>)</name>
+   <name><? echo getName($row); ?></name>
    <styleUrl>#randomColorIcon</styleUrl>
    <visibility>1</visibility>
    <description>
     <![CDATA[
-     <img src="<? echo getImage(str_replace(' ', '_', $row['image']), 200) ?>" />
+     <? echo getImage($row); ?>
      <ul>
-      <li>objrijksnr - <? echo $row['objrijksnr'] ?></li>
-      <li>woonplaats - <? echo $row['woonplaats'] ?></li>
-      <li>adres - <? echo $row['adres'] ?></li>
-      <li>objectnaam - <? echo $row['objectnaam'] ?></li>
-      <li>type_obj - <? echo $row['type_obj'] ?></li>
-      <li>oorspr_functie - <? echo $row['oorspr_functie'] ?></li>
-      <li>bouwjaar - <? echo $row['bouwjaar'] ?></li>
-      <li>architect - <? echo $row['architect'] ?></li>
-      <li>cbs_tekst - <? echo $row['cbs_tekst'] ?></li>
-      <li>RD_x - <? echo $row['RD_x'] ?></li>
-      <li>RD_y - <? echo $row['RD_y'] ?></li>
-      <li>lat - <? echo $row['lat'] ?></li>
-      <li>lon - <? echo $row['lon'] ?></li>
-      <li>image - <? echo $row['image'] ?></li>
-      <li>source - <? echo $row['source'] ?></li>
-      <li>changed - <? echo $row['changed'] ?></li>
+      <? if ($row['objectnaam'] != '') { ?>
+      <li>Naam - <? echo processWikitext($row['objectnaam'], 1); ?></li>
+      <? } ?>
+      <li>Rijksmonument  - <? echo $row['objrijksnr'] ?></li>
+      <? if ($row['adres'] != '') { ?>
+      <li>Adres - <? echo $row['adres'] ?></li>
+      <? } ?>
+      <li>Plaats - <? echo $row['woonplaats'] ?></li>
+      <li>Type object - <? if ($row['type_obj']=='A') {
+      ?>Archeologisch monument<?
+      } else {
+      ?>Gebouw<?
+      }?>
+      <li>Oorspr functie - <? echo $row['oorspr_functie'] ?></li>
+      <? if ($row['bouwjaar'] != '') { ?>
+      <li>Bouwjaar - <? echo $row['bouwjaar'] ?></li>
+      <? } if ($row['architect'] != '') { ?>
+      <li>Architect - <? echo $row['architect'] ?></li>
+      <? } if ($row['cbs_tekst'] != '') { ?>
+      <li>CBS tekst - <? echo $row['cbs_tekst'] ?></li>
+      <? } ?>
+      <li>Rijksdriehoekscoordinaten - <? echo $row['RD_x'] ?>, <? echo $row['RD_y'] ?></li>
+      <li>Lengtegraad - <? echo $row['lat'] ?></li>
+      <li>Breedtegraad - <? echo $row['lon'] ?></li>
+      <li>Bron van deze informatie - <? echo $row['source'] ?></li>
+      <li>Laatste wijziging - <? echo $row['changed'] ?></li>
      </ul>
-<? echo $bbox;
-	       echo "A" . $coordinaten[0] . "\n";
-	               echo "B" . $coordinaten[1];
-		               echo "C" . $coordinaten[2];
-			               echo $coordinaten[3];
-?> 
     ]]>
    </description>
    <Point>
