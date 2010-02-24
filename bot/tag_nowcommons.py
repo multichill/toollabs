4de8a1b2b74503e1bfb0982c2d113c043b7138c1
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2010-02-24 19:59:59 +0000

    Work on more wikipedias (fywp to start with)
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@140 7e777f60-21ef-46b6-b16d-5948f447dc97

--- bot/tag_nowcommons.py
+++ bot/tag_nowcommons.py
@@ -8,20 +8,60 @@ import sys
 sys.path.append("../pywikipedia")
 import wikipedia, MySQLdb, config
 
+skips = {}
+skips['wikipedia'] = {}
+skips['wikipedia']['en'] = [u'NowCommons',
+			    u'CommonsNow',
+			    u'Nowcommons',
+			    u'NowCommonsThis',
+			    u'Nowcommons2',
+			    u'NCT',
+			    u'Nowcommonsthis',
+			    u'Moved to commons',
+			    u'Now Commons',
+			    u'Now at commons',
+			    u'Db-nowcommons',
+			    u'WikimediaCommons',
+			    u'Now commons',
+			    u'Do not move to Commons',
+			    u'KeepLocal',
+			    u'Keeplocal',
+			    u'NoCommons',
+			    u'Nocommons',
+			    u'NotMovedToCommons',
+			    u'Nmtc',
+			    u'Not moved to Commons',
+			    u'Notmovedtocommons',
+			    ]
+skips['wikipedia']['fy'] = [u'NowCommons',
+			    u'Nowcommons',
+			    ]
+skips['_default'] = [u'NowCommons']
+
 def connectDatabase():
     '''
     Connect to the mysql database, if it fails, go down in flames
     '''
-    conn = MySQLdb.connect('enwiki-p.db.toolserver.org', db='enwiki_p', user = config.db_username, passwd = config.db_password, charset='utf8', use_unicode=True)
+    site = wikipedia.getSite()
+    language = site.language()
+    family = site.family.name
+    conn_start = MySQLdb.connect('sql.toolserver.org', db='toolserver', user = config.db_username, passwd = config.db_password, charset='utf8', use_unicode=True)
+    cursor_start = conn_start.cursor()
+    
+    query_start = u"""SELECT dbname, server FROM wiki WHERE lang='%s' AND family='%s' LIMIT 1"""
+    cursor_start.execute(query_start % (language, family))
+    (dbname, server) = cursor_start.fetchone() 
+    conn_start.close()
+
+    conn = MySQLdb.connect('sql-s' + str(server) + '.toolserver.org', db=dbname, user = config.db_username, passwd = config.db_password, charset='utf8', use_unicode=True)
     cursor = conn.cursor()
     return (conn, cursor)
-
 
 def getImagesToNowcommons(cursor):
     '''
     Get all images which exist at Commons with the same hash
     '''
-    query =u"""SELECT wi.img_name, ci.img_name FROM enwiki_p.image AS wi
+    query =u"""SELECT wi.img_name, ci.img_name FROM image AS wi
                JOIN commonswiki_p.image AS ci
                ON (wi.img_size=ci.img_size AND wi.img_width=ci.img_width AND wi.img_height=ci.img_height AND wi.img_sha1=ci.img_sha1) """
 
@@ -32,35 +72,22 @@ def getImagesToNowcommons(cursor):
 	    
 
 def tagNowCommons(wImage, cImage):
+    site = wikipedia.getSite()
+    language = site.language()
+    family = site.family.name
+
     imagepage = wikipedia.ImagePage(wikipedia.getSite(), wImage)
     if not imagepage.exists() or imagepage.isRedirectPage():
 	return
-    skips = [u'NowCommons',
-	     u'CommonsNow',
-	     u'Nowcommons',
-             u'NowCommonsThis',
-             u'Nowcommons2',
-             u'NCT',
-             u'Nowcommonsthis',
-             u'Moved to commons',
-             u'Now Commons',
-             u'Now at commons',
-             u'Db-nowcommons',
-             u'WikimediaCommons',
-             u'Now commons',
-             u'Do not move to Commons',
-	     u'KeepLocal',
-	     u'Keeplocal',
-             u'NoCommons',
-	     u'Nocommons',
-             u'NotMovedToCommons',
-             u'Nmtc',
-             u'Not moved to Commons',
-             u'Notmovedtocommons',
-	    ]
+
+    if skips.get(family) and skips.get(family).get(language):
+	localskips = skips.get(family).get(language)
+    else:
+	localskips = skips.get('_default')
+
     for template in imagepage.templates():
 	title = template.replace(u'_', u' ').strip()
-	if title in skips:
+	if title in localskips:
 	    return
     text = imagepage.get()
     oldtext = text
@@ -77,7 +104,7 @@ def main():
     '''
     The main loop
     '''
-    wikipedia.setSite(wikipedia.getSite(u'en', u'wikipedia'))
+    wikipedia.handleArgs()
     conn = None
     cursor = None
     (conn, cursor) = connectDatabase()
