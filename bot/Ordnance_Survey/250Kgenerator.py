be61c48def22094c380925bbe055189d0f2a4c29
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2010-06-06 13:41:30 +0000

    First start, now move some stuff to the lib
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@158 7e777f60-21ef-46b6-b16d-5948f447dc97

--- bot/Ordnance_Survey/250Kgenerator.py
+++ bot/Ordnance_Survey/250Kgenerator.py
@@ -5,90 +5,59 @@ Bot to upload geograph images from the Toolserver to Commons
 
 '''
 import sys, os.path, hashlib, base64, MySQLdb, glob, re, urllib, time
+import OSlib
 sys.path.append("/home/multichill/pywikipedia")
 import wikipedia, config, query
 import xml.etree.ElementTree, shutil
 import imagerecat
 import MySQLdb.converters
 
-def getDescription(metadata):
+def processSquare(square, squares, scale, sourcedir, basefilename, destinationdir):
+    '''
+    Generate a description (same for jpg and tif) and copy all the files
+    '''
+    description = getDescription(square, squares, scale, basefilename)
+    print description
+
+    time.sleep(10)
+    #outputDescriptionFile(destinationdir + basefilename + square + u'.txt', description) 
+
+    # Copy the tif file
+    #shutil.copy(unicode(sourcedir + square + u'.tif'), unicode(destinationdir + basefilename + square + u'.tif'))
+
+    # Copy the jpg file
+    #shutil.copy(unicode(sourcedir + square + u'.jpg'), unicode(destinationdir + basefilename + square + u'.jpg'))
+
+def getDescription(square, squares, scale, basefilename):
     '''
     Create the description of the image based on the metadata
     '''
     description = u''
-
-    description = description + u'== {{int:filedesc}} ==\n'
-    description = description + u'{{Information\n'
-    informationDescription = metadata.get('title').strip()
-    if informationDescription[-1:].isalnum():
-	informationDescription = informationDescription + u'.'
-    if not metadata['comment']==u'':
-	informationDescription = informationDescription + u' ' + metadata['comment']
-    description = description + u'|description={{en|1=' +informationDescription + u'}}\n'
-    description = description + u'|date='
-    if metadata.get('imagetaken') and not metadata.get('imagetaken')==u'0000-00-00':
-	description = description + metadata.get('imagetaken').replace(u'-00', u'') + u'\n'
-    else:
-	description = description + u'{{Unknown}}\n'
-    description = description + u'|source=From [http://www.geograph.org.uk/photo/' + str(metadata.get('id')) + u' geograph.org.uk]\n'
-    description = description + u'|author=[http://www.geograph.org.uk/profile/' + str(metadata.get('user_id')) + u' ' + metadata.get('realname') + u']\n'
-    description = description + u'|permission=\n'
-    description = description + u'|other_versions=\n'
+    
+    description = description + u'{{subst:Commons:Batch uploading/Ordnance Survey/Template\n'
+    description = description + u'|subst=subst:\n'
+    description = description + u'|square=' + square + '\n'
+    description = description + u'|scale=' + scale + '\n'
+    description = description + u'|scale=' + basefilename.replace(u'_', u' ') + '\n'
+    description = description + u'|nw_square=' + getNextSquare(getNextSquare(square, 'n'), u'w')  + '\n'
+    description = description + u'|n_square=' + getNextSquare(currentSquare=square, direction='n')  + '\n'
+    description = description + u'|ne_square=' + getNextSquare(getNextSquare(square, 'n'), u'e')  + '\n'
+    description = description + u'|w_square=' + getNextSquare(square, 'w')  + '\n'
+    description = description + u'|e_square=' + getNextSquare(square, 'e')  + '\n'
+    description = description + u'|sw_square=' + getNextSquare(getNextSquare(square, 's'), u'w')  + '\n'
+    description = description + u'|s_square=' + getNextSquare(square, 's')  + '\n'
+    description = description + u'|se_square=' + getNextSquare(getNextSquare(square, 's'), u'e')  + '\n'
     description = description + u'}}\n'
-    # Location, add heading
-    description = description + u'{{Location dec|' + str(metadata.get('wgs84_lat'))
-    description = description + u'|' + str(metadata.get('wgs84_long'))
-    # ADD HEADING HERE
-    if not metadata.get('view_direction') == -1:
-	description = description + u'|heading:' + str(metadata.get('view_direction'))
-    description = description + u'}}\n'
-    description = description + u'\n'
-    description = description + u'== {{int:license}} ==\n'
-    description = description + u'{{Geograph|' + str(metadata.get('id'))
-    description = description + u'|' + metadata.get('realname') +'}}\n'
-    description = description + u'\n'
 
     return description
-
-def getTitle(metadata):
-    '''
-    Build a valid title for the image to be uploaded to.
-    '''
-    title = metadata.get('title') + u' - geograph.org.uk - ' + str(metadata.get('id'))
-
-    title = re.sub(u"[<{\\[]", u"(", title)
-    title = re.sub(u"[>}\\]]", u")", title)
-    title = re.sub(u"[ _]?\\(!\\)", u"", title)
-    title = re.sub(u",:[ _]", u", ", title)
-    title = re.sub(u"[;:][ _]", u", ", title)
-    title = re.sub(u"[\t\n ]+", u" ", title)
-    title = re.sub(u"[\r\n ]+", u" ", title)
-    title = re.sub(u"[\n]+", u"", title)
-    title = re.sub(u"[?!]([.\"]|$)", u"\\1", title)
-    title =  re.sub(u"[&]", u"and", title)
-    title = re.sub(u"[#%?!]", u"^", title)
-    title = re.sub(u"[;]", u",", title)
-    title = re.sub(u"[/+\\\\:]", u"-", title)
-    title = re.sub(u"--+", u"-", title)
-    title = re.sub(u",,+", u",", title)
-    title = re.sub(u"[-,^]([.]|$)", u"\\1", title)
-    title = title.replace(u" ", u"_")   
     
-    return title
-        
-
-
-def getFileId(file):
-    dirname = os.path.dirname(file)
-    filename = os.path.basename(file)
-    baseFilename, extension = os.path.splitext(filename)
-    return int(baseFilename)
 
 def outputDescriptionFile(filename, description):
     f = open(filename, "w")
     f.write(description.encode("UTF-8"))
     f.close()
     return
+
 
 def main(args):
     '''
@@ -99,16 +68,17 @@ def main(args):
 
     sourcedir=u'/mnt/user-store/OS_OpenData/1_250_000_Scale_Raster/data/'
     destinationdir=u'/mnt/user-store/OS_OpenData/1_250_000_Scale_Raster/output/'
-
-    gridsquares = []
+    basefilename=u'Ordnance_Survey_1-250000_-_'
+    scale=u'250.000'
+    squares = []
 
     for sourcefilename in glob.glob(sourcedir + u"*.tif"):
 	square = sourcefilename.replace(sourcedir, u'').replace(u'.tif', u'')
-	gridsquares.append(square)
+	squares.append(square)
 
-    for square in gridsquares:
+    for square in squares:
 	print square
-	#processSquare(square, squares, sourcedir, destinationdir)
+	processSquare(square, squares, scale, sourcedir, basefilename, destinationdir)
 	
 
     '''
