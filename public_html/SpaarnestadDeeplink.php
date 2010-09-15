0a1940ae9a91126bc395fc3c5ec68ea74e00b624
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2010-09-15 17:15:44 +0000

    Simple hack to deeplink to Spaarnestad
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@191 7e777f60-21ef-46b6-b16d-5948f447dc97

---
+++ public_html/SpaarnestadDeeplink.php
@@ -0,0 +1,10 @@
+<form action="http://www.spaarnestadphoto.nl/component/option,com_memorix/task,result/searchplugin,simple/Itemid,60/lang,nl/" method="post" id="spaarnestadsearch">
+<input name="trefwoord" id="mod_sfa_searchmemorix" maxlength="20" alt="search" class="textfield" type="text" size="20" value="<? echo $_GET['trefwoord'] ?>" />
+<input type="submit" value="Zoek" class="button" />  <input type="hidden" name="option" value="com_memorix" />
+<input type="hidden" name="Itemid" value="60" />	
+</form>
+<script language="JavaScript" type="text/javascript">
+<!--
+document.getElementById('spaarnestadsearch').submit();
+//-->
+</script>
