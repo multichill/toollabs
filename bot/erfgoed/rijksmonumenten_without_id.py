a3c41e45b765a0bbacc398bbda015eb20dafba80
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2010-10-16 11:09:30 +0000

    Tool to make list of rijksmonumenten images without an id.
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@196 7e777f60-21ef-46b6-b16d-5948f447dc97

---
+++ bot/erfgoed/rijksmonumenten_without_id.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+'''
+
+Make a gallery of rijksmonumenten without an id at Commons
+
+FIXME: Encoding issues.
+
+'''
+import sys
+sys.path.append("/home/multichill/pywikipedia")
+import wikipedia, config, pagegenerators, catlib
+import re, imagerecat
+import MySQLdb, config, time
+
+def connectDatabase():
+    '''
+    Connect to the rijksmonumenten mysql database, if it fails, go down in flames
+    '''
+    conn = MySQLdb.connect('sql.toolserver.org', db='p_erfgoed_p', user = config.db_username, passwd = config.db_password, use_unicode=True, charset='utf8')
+    cursor = conn.cursor()
+    return (conn, cursor)
+
+def connectDatabase2():
+    '''
+    Connect to the commons mysql database, if it fails, go down in flames
+    '''
+    conn = MySQLdb.connect('commonswiki-p.db.toolserver.org', db='commonswiki_p', user = config.db_username, passwd = config.db_password, use_unicode=True, charset='utf8')
+    cursor = conn.cursor()
+    return (conn, cursor)
+
+def getRijksmonumentenWithPhoto(conn, cursor):
+    result = {}
+
+    query = u"""SELECT image, objrijksnr FROM monumenten WHERE NOT image=''""";
+
+
+    cursor.execute(query)
+
+    while True:
+	try:
+	    row = cursor.fetchone()
+	    (image, objrijksnr) = row
+	    result[image] = objrijksnr
+	except TypeError:
+	    break
+
+    return result
+
+def getRijksmonumentenWithoutTemplate(conn, cursor):
+    result = []
+
+    query = u"""SELECT DISTINCT(page_title) FROM page JOIN categorylinks ON page_id=cl_from WHERE page_namespace=6 AND page_is_redirect=0 AND cl_to LIKE 'Rijksmonumenten\_in\_%' AND NOT EXISTS(SELECT * FROM templatelinks WHERE page_id=tl_from AND tl_namespace=10 AND tl_title='Rijksmonument') ORDER BY page_title ASC"""
+
+    cursor.execute(query)
+
+    while True:
+        try:
+            row = cursor.fetchone()
+            (image,) = row
+            result.append(image.decode('utf-8'))
+        except TypeError:
+            break
+
+    return result
+
+
+def main():
+    # Connect database, we need that
+    conn = None
+    cursor = None
+    (conn, cursor) = connectDatabase()
+    (conn2, cursor2) = connectDatabase2()
+
+    withPhoto = getRijksmonumentenWithPhoto(conn, cursor)
+    withoutTemplate = getRijksmonumentenWithoutTemplate(conn2, cursor2)
+
+    text = u'<gallery>\n'
+    for image in withoutTemplate:
+	if withPhoto.get(image):
+	    text = text + u'File:%s|{{tl|Rijksmonument|%s}}\n' % (image, withPhoto.get(image))
+	else:
+	     text = text + u'File:%s\n' % (image,)
+	    
+    text = text + u'</gallery>' 
+    comment = u'Plaatjes om id bij te zoeken'
+    
+    site = wikipedia.getSite(u'nl', u'wikipedia')
+    page = wikipedia.Page(site, u'Wikipedia:Wikiproject/Erfgoed/Nederlandse Erfgoed Inventarisatie/Foto\'s zonder id')
+    page.put(text, comment)
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        wikipedia.stopme()
