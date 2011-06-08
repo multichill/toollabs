0a9bd3f54ea6e9ffb312d7c8797d4fe0e93e6503
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2011-06-08 16:35:47 +0000

    First two steps are working
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@251 7e777f60-21ef-46b6-b16d-5948f447dc97

--- public_html/Europeana/index.php
+++ public_html/Europeana/index.php
@@ -1,9 +1,12 @@
 <?php
-// FIXME: Put it somewhere less public like mysql key
-$wskey = "QYLCOXOPNF";
+$title = "Europeana search tool";
+$modified = date ("G:i, n F Y", getlastmod());
 
-$query = $_GET['query'];
-$link = $_GET['link'];
+$config = array();
+// FIXME: Put it somewhere less public like mysql key
+$config['wskey'] = "QYLCOXOPNF";
+$config['query'] = urlencode($_GET['query']);
+$config['record'] = $_GET['record'];
 
 function getTopOfPage() {
 	$result = array();
@@ -17,18 +20,100 @@ function getBottomOfPage() {
 	return implode("\n", $result);
 }
 
-function buildStartScreen() {
+function buildStartScreen($query) {
 	/* Build a starting search form */
 	$result = array();
+	$result[] = "<H2>Europeana search tool query</H2>";
+	$result[] = '<img alt="File:Europeana logo 3 eu black.png" src="http://upload.wikimedia.org/wikipedia/commons/b/be/Europeana_logo_3_eu_black.png" width="128" height="70" />';
+	$result[] = '<p>With this tool you can find images in Europeana to be uploaded to Wikimedia Commons</p>';
 	$result[] = '<form method="get" accept-charset="UTF-8" name="europeana-search" id="europeana-search">';
-	$result[] = '<input class="search-input" name="query" id="query" type="text" title="Search term(s)" value="" maxlength="75"/>';
+	$result[] = '<input class="search-input" name="query" id="query" type="text" title="Search term(s)" value="' . urldecode($query) . '" maxlength="75"/>';
 	$result[] = '<input id="submit_search" type="submit" class="button" value="Search" />';
 	$result[] = '</form>';
 	return implode("\n", $result);
 }
 
-function getRecordMetadata($link, $wskey) {
+function getGalleryPage($query) {
+	$result = array();
+	//$result[] = '<ul class="gallery">';
+	$result[]= buildStartScreen($query);
+
+	global $config;
+	$url = "http://api.europeana.eu/api/opensearch.rss?searchTerms=" . $query . "&wskey=" . $config['wskey'];
+	//echo $url;
+	$xml = new XMLReader();
+
+	$xml->open($url);
+
+	while($xml->read() && $xml->name!=="channel");
+	$xml->read();
+	while($xml->read()){
+		//echo "|" . $xml->name . "=" . $xml->readstring() . $xml->depth . "\n";
+		if ($xml->depth==2) {
+		    //echo "|" . $xml->name . "=" . $xml->readstring() . $xml->depth . "\n";
+		    if ($xml->nodeType==1 && $xml->name=="description") {
+			    $result[] = "<H2>" . $xml->readstring() . "</H2>";
+		    } elseif ($xml->nodeType==1 && $xml->name=="opensearch:totalResults") {
+			    $result[] = "<p>Total number of files found:" . $xml->readstring() . '</p><ul class="gallery">';
+		    } elseif ($xml->nodeType==1 && $xml->name=="item") {
+			    $result[] = getGalleryItem($xml);
+		    }
+		}
+	}
+	//if ($xml->nodeType==1 && $xml->depth==5)
+	$result[] = "</ul>";
+	return implode("\n", $result);
+}
+
+function getGalleryItem($xml) {
+	$record = "";
+	$image = "";
+	$title = "";
+
+	while($xml->read() && $xml->depth!==3);
+	while($xml->read() && ($xml->depth==3 || $xml->depth==4)) {
+		//echo "|" . $xml->name . "=" . $xml->readstring() . $xml->depth . "\n";
+		if ($xml->depth==3) {
+			if ($xml->nodeType==1 && $xml->name=="title") {
+				$title = $xml->readstring();
+			} elseif ($xml->nodeType==1 && $xml->name=="link") {
+				$link = $xml->readstring();
+				// Do somethign with explode
+				$pieces = explode(".srw?wskey", $link);
+				$record = str_replace("http://www.europeana.eu/portal/record/", "", $pieces[0]);
+			} elseif ($xml->nodeType==1 && $xml->name=="enclosure") {
+				$image = $xml->getAttribute("url");
+			}
+		}
+	}
+
+	$result = array();
+	$result[] = "";
+
+	$result[] = '<li class="gallerybox" style="width: 155px">';
+	$result[] = '<div style="width: 155px">';
+	$result[] = '<div class="thumb" style="width: 150px; height: 150px;">';
+	$result[] = '<div style="margin:15px auto;"><a href=?record=' . $record . ' class="image"><img alt="" src="' . $image . '" width="83" height="120" /></a></div>';
+	$result[] = '</div>';
+	$result[] = '<div class="gallerytext">';
+	$result[] = '<p>' . $title . '</p>';
+	$result[] = '</div>';
+	$result[] = '</div>';
+	$result[] = '</li>';
+
+	return implode("\n", $result);
+}
+
+function getSingleRecordPage($record) {
+	$result = array();
+	$metadata = getRecordMetadata($record);
+	$result[] = getTemplateCode("Europeana bla", $metadata);
+	return implode("\n", $result);
+}
+
+function getRecordMetadata($record) {
 	/* $link should point to an Europeana XML record */
+	$link = "http://www.europeana.eu/portal/record/" .$record . ".srw?wskey=QYLCOXOPNF";
 
 	$xml = new XMLReader();
 	// FIXME: Use record and key
@@ -68,22 +153,26 @@ function getTemplateCode ($templateName, $metadata) {
 	return implode($result);
 }
 
-function main() {
+function main($config) {
 	$result = array();
-	$result[]= getTopOfPage();
-	if (isset($link)) {
-	        $result[] = getSingleRecordPage($link);
-	} elseif (isset($query)) {
-	        $result[] = getGalleryPage($query);
+	//$result[]= getTopOfPage();
+	//include("../inc/header.php");
+	if (!empty($config['record'])) {
+	        $result[] = getSingleRecordPage($config['record']);
+	} elseif (!empty($config['query'])) {
+	        $result[] = getGalleryPage($config['query']);
 	} else {
-	        $result[] = buildStartScreen();
+	        $result[] = buildStartScreen("");
 	}
 
-	$result[] = getBottomOfPage();
+	//$result[] = getBottomOfPage();
 	// Only location in the whole program to output something
 	echo implode("\n", $result);
+	//include("../inc/footer.php");
 }
-main();
+include("../inc/header.php");
+main($config);
+include("../inc/footer.php");
 /*
 	echo buildStartScreen();
 $link = "http://www.europeana.eu/portal/record/09428/DA8F6B70E65DD37E7A1D9D550B7D500009E9CC6A.srw?wskey=QYLCOXOPNF";
