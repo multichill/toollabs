e57b92f58f93c2a8ac1f13b869ed453820bd97a1
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2011-06-08 12:46:37 +0000

    Iframe crap. Keeping it for reference
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@249 7e777f60-21ef-46b6-b16d-5948f447dc97

--- public_html/Europeana/index.php
+++ public_html/Europeana/index.php
@@ -3,8 +3,8 @@
 $query = $_GET['query'];
 ?>
 <html>
-<frameset rows="10%,90%">
-  <frame name="toolbar" src="toolbar.php" />
-  <frame name="Europeana" src="http://www.europeana.eu/portal/search.html?query=<?php echo $query ?>" />
+Bla die bla<BR />
+More <BR />
+<iframe name="Europeana" width="100%" height="90%" src="http://www.europeana.eu/portal/search.html?query=<?php echo $query ?>" />
 </frameset>
 </html>
