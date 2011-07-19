b7ebde0a1b56cd3b98b49c904ac20507db543dd0
multichill <multichill@7e777f60-21ef-46b6-b16d-5948f447dc97>
2011-07-19 18:46:33 +0000

    Get rid of flickrripper import.
    git-svn-id: https://svn.toolserver.org/svnroot/multichill@262 7e777f60-21ef-46b6-b16d-5948f447dc97

--- bot/NARA/NARA_uploader.py
+++ bot/NARA/NARA_uploader.py
@@ -12,7 +12,7 @@ The bot uses http://toolserver.org/~slakr/archives.php to get the description
 import sys, os.path, hashlib, base64, glob, re, urllib, time
 sys.path.append("..\..\pywikipedia")
 import wikipedia, config, query
-import shutil, socket, flickrripper
+import shutil, socket
 
 def getRecords(textfile):
     result = {}
