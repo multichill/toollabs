40e3a65ecc60d891af3f975e6ded40f3bf739120
multichill <maarten@mdammers.nl>
2016-10-23 16:29:26 +0200

    Some fixes for the BPN bot. Also added reporting.

--- bot/wikidata/biografisch_finder.py
+++ bot/wikidata/biografisch_finder.py
@@ -28,26 +28,31 @@ class BPNFinderBot:
     """
     A bot to add missing links based on Biografisch portaal van Nederland
     """
-    def __init__(self, generator, otherproperty, idregex):
+    def __init__(self, generator, otherproperty, idregex, report=None):
         """
         Arguments:
         :param generator     - A generator of BPN id's
         :param otherproperty - The other property to work on (for example P650 for RKDartists)
         :param idregex     - The regex to extract the id from a link
+        :param reportpage  - Name of the page to report the missing ones on
 
         """
         self.generator = generator
         self.bpnproperty = u'P651'
+        self.bpnbaseurl = u'http://www.biografischportaal.nl/persoon/%s?'
         self.otherproperty = otherproperty
         self.idregex = idregex
+        self.report = report
 
         self.repo = pywikibot.Site().data_repository()
         # Has both BPN and RKDartists
         self.completed = self.buildIdCache(self.bpnproperty, extraproperty=self.otherproperty)
         # Has BPN, but not the other property
         self.missingother = self.buildIdCache(self.bpnproperty, minusproperty=self.otherproperty)
-        # Does not have BGP, but does have the other property
-        self.missingbpn = self.buildIdCache(self.otherproperty, minusproperty=self.bpnproperty)
+        # Has the other property, but might not have BNP. No filtering because of redirects
+        self.missingbpn = self.buildIdCache(self.otherproperty) #, minusproperty=self.bpnproperty)
+
+        self.missingtext = u'This is the report for Biografisch Portaal Nederland and %s\n' % (otherproperty,)
 
     def buildIdCache(self, property, extraproperty=None, minusproperty=None):
         '''
@@ -89,6 +94,15 @@ class BPNFinderBot:
             else:
                 self.addBPNProperty(bpnid)
 
+        self.missingtext = self.missingtext + u'\n\n[[Category:User:Multichill]]\n'
+        if self.report:
+            summary = u'Missing report'
+            pywikibot.output(self.missingtext)
+
+            reportpage = pywikibot.Page(self.repo, title=self.report)
+            reportpage.put(self.missingtext , summary=summary)
+
+
     def getOtherPropertyId(self, bpnid):
         """
         Helper function to retrieve the other property for bpnid
@@ -96,8 +110,7 @@ class BPNFinderBot:
         :return: The id of the other property or False if it isn't found
         """
 
-        bpnurl = u'http://www.biografischportaal.nl/persoon/%s?' % (bpnid,)
-
+        bpnurl = self.bpnbaseurl % (bpnid,)
         # Do some checking if it actually exists?
         bpnPage = requests.get(bpnurl, verify=False)
 
@@ -105,6 +118,7 @@ class BPNFinderBot:
         if idmatch:
             return idmatch.group(1)
         pywikibot.output(u'Other id not found for %s' % (bpnid,))
+        self.missingtext = self.missingtext + u'* On page [%s %s] unable to extract other id\n' % (bpnurl, bpnid)
         return False
 
     def addOtherProperty(self, bpnid):
@@ -115,10 +129,10 @@ class BPNFinderBot:
         :return: Boolean with True if the otherid is on the Wikidata item (either already there or added)
         """
         otherid = self.getOtherPropertyId(bpnid)
-        bpnurl = u'http://www.biografischportaal.nl/persoon/%s?' % (bpnid,)
+        bpnurl = self.bpnbaseurl % (bpnid,)
 
         # Do some checking if it actually exists?
-        #bpnPage = requests.get(bpnurl, verify=False)
+        #bpnPage = requests.get(selfbpnurl, verify=False)
 
         #idmatch = re.search(self.idregex, bpnPage.text)
         if otherid:
@@ -149,11 +163,13 @@ class BPNFinderBot:
         :return: boolean with True if the bpnid is on a Wikidata item (either already there or added)
         """
         otherid = self.getOtherPropertyId(bpnid)
+        bpnurl = self.bpnbaseurl % (bpnid,)
 
         if not otherid:
             return False
         if otherid not in self.missingbpn:
             pywikibot.output(u'BPN %s gave other id %s, but nothing found for this id' % (bpnid, otherid))
+            self.missingtext = self.missingtext + u'* On page [%s %s] found %s but couldn\'t find Wikidata item\n' % (bpnurl, bpnid, otherid)
             return False
 
         # We found an item that has the other property, but not BPN
@@ -223,7 +239,7 @@ def biografischportaalGenerator(source_id):
     nummatch =  re.search(numregex, firstPage.text)
     total = int(nummatch.group(1).replace('.', u''))
     step = 30
-    for i in range(300, total+step, step):
+    for i in range(0, total+step, step):
         searchPage = requests.get(baseurl % (source_id, i), verify=False)
         for bpnid in re.finditer(bpnidregex, searchPage.text):
             yield bpnid.group(1)
@@ -247,6 +263,7 @@ def main(*args):
                }
 
     source = None
+    report = None
     for arg in pywikibot.handle_args(args):
         if arg.startswith('-source:'):
             if len(arg) == 8:
@@ -254,13 +271,18 @@ def main(*args):
                         u'Please enter the source property you want to work on:')
             else:
                 source = arg[8:]
-
+        elif arg.startswith('-report:'):
+            if len(arg) == 8:
+                report = pywikibot.input(
+                        u'Please enter the name of the page to report on:')
+            else:
+                report = arg[8:]
     repo = pywikibot.Site().data_repository()
 
     # Source was passed on the commandline. Only work on that one
     if source and source in sources:
         idgenerator = biografischportaalGenerator(sources.get(source).get(u'source_id'))
-        bpnFinderBot = BPNFinderBot(idgenerator, otherproperty=source, idregex=sources.get(source).get(u'idregex'))
+        bpnFinderBot = BPNFinderBot(idgenerator, otherproperty=source, idregex=sources.get(source).get(u'idregex'), report=report)
         bpnFinderBot.run()
     else:
         # Let's work on all of them
