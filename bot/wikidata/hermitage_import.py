1a894b321ea43489ca4b05c05223932ca94c31a7
multichill <maarten@mdammers.nl>
2016-12-05 11:39:13 +0100

    Clean up and expansion of Hermitage import bot. Looks like I missed quite a bit of data and a lot of new paintings became available.

--- bot/wikidata/hermitage_import.py
+++ bot/wikidata/hermitage_import.py
@@ -7,54 +7,34 @@ Bot to import Hermitage paintings. No api, no csv, just plain old screen scrapin
 * Grab individual paintings like https://www.hermitagemuseum.org/wps/portal/hermitage/digital-collection/01.+Paintings/25685/ (remove the language)
 
 
-The bot takes three csv files:
-* One with the artists
-* One with the type of works
-* One with all the works
-
-Combines these and uploads the data to Wikidata
 """
 import artdatabot
-#import json
 import pywikibot
-#from pywikibot import pagegenerators
-import urllib2
+import requests
 import re
-#import pywikibot.data.wikidataquery as wdquery
-#import datetime
 import HTMLParser
-#import posixpath
-#from urlparse import urlparse
-#from urllib import urlopen
-#import hashlib
-#import io
-#import base64
-#import upload
-#import tempfile
-import os
-import csv
-
-
 
 def getHermitageGenerator():
     '''
     Generator to return Hermitage paintings
     '''
 
+    # Use one session for everything
+    session = requests.Session()
+
     # 1 - 367
     searchBaseUrl = u'https://www.hermitagemuseum.org/wps/portal/hermitage/explore/collections/col-search/?lng=en&p1=category:%%22Painting%%22&p15=%s'
     baseUrl = u'https://www.hermitagemuseum.org%s'
     htmlparser = HTMLParser.HTMLParser()
 
-    for i in range(1, 378):
+    for i in range(1, 432):
         searchUrl = searchBaseUrl % (i,)
         print searchUrl
-        searchPage = urllib2.urlopen(searchUrl)
-        searchPageData = searchPage.read()
+        searchPage = session.get(searchUrl, verify=False)
 
-        searchRegex = u'<div class="her-search-results-row">[\r\n\s]+<div class="her-col-35 her-search-results-img">[\r\n\s]+<a href="(/wps/portal/hermitage/digital-collection/01.+Paintings/\d+/)?lng=en"'
+        # searchRegex = u'<div class="her-search-results-row">[\r\n\s]+<div class="her-col-35 her-search-results-img">[\r\n\s]+<a href="(/wps/portal/hermitage/digital-collection/01.+Paintings/\d+/)?lng=en"'
         searchRegex = u'<div class="her-search-results-row">[\r\n\s]+<div class="her-col-35 her-search-results-img">[\r\n\s]+<a href="(/wps/portal/hermitage/digital-collection/01\.\+Paintings/\d+/)\?lng=en"'
-        matches = re.finditer(searchRegex, searchPageData)
+        matches = re.finditer(searchRegex, searchPage.text)
         for match in matches:
             metadata = {}
 
@@ -67,26 +47,22 @@ def getHermitageGenerator():
             metadata['url_en'] = '%s?lng=en' % (metadata['url'],)
             metadata['url_ru'] = '%s?lng=ru' % (metadata['url'],)
 
-            itemPageEn = urllib2.urlopen(metadata['url_en'])
-            itemPageRu = urllib2.urlopen(metadata['url_ru'])
+            itemPageEn = session.get(metadata['url_en'], verify=False)
+            itemPageRu = session.get(metadata['url_ru'], verify=False)
             print metadata['url_en']
-            itemPageEnData = unicode(itemPageEn.read(), u'utf-8')
-            itemPageRuData = unicode(itemPageRu.read(), u'utf-8')
 
-            #print itemPageEnData
             headerRegex = u'<header>[\r\n\s]+<h3>([^<]*)</h3>[\r\n\s]+<h1>([^<]*)</h1>[\r\n\s]+<p>([^<]*)</p>[\r\n\s]+</header>'
-            matchEn = re.search(headerRegex, itemPageEnData)
+            matchEn = re.search(headerRegex, itemPageEn.text)
             if not matchEn:
                 pywikibot.output(u'The data for this painting is BORKED!')
                 continue
 
-            matchRu = re.search(headerRegex, itemPageRuData)
+            matchRu = re.search(headerRegex, itemPageRu.text)
 
 
             metadata['title'] = { u'en' : htmlparser.unescape(matchEn.group(2)),
                                   u'ru' : htmlparser.unescape(matchRu.group(2)), 
                                   }
-            #pywikibot.output(metadata.get('title'))
 
             painterName = matchEn.group(1)
 
@@ -98,120 +74,64 @@ def getHermitageGenerator():
                 if painterMatch:
                     painterName = '%s %s' % (painterMatch.group(2), painterMatch.group(1),)
                     continue
-            metadata['creatorname'] = painterName
 
-            metadata['description'] = { u'nl' : u'%s van %s' % (u'schilderij', painterName,),
-                                        u'en' : u'%s by %s' % (u'painting', painterName,),
-                                        }
+            if painterName==u'Unknown artist' or not painterName.strip():
+                metadata['creatorname'] = u'anonymous'
+                metadata['description'] = { u'nl' : u'schilderij van anonieme schilder',
+                                            u'en' : u'painting by anonymous painter',
+                                            }
+                metadata['creatorqid'] = u'Q4233718'
+            else:
+                metadata['creatorname'] = painterName
 
-            #pywikibot.output(metadata.get('description'))
+                metadata['description'] = { u'nl' : u'%s van %s' % (u'schilderij', painterName,),
+                                            u'en' : u'%s by %s' % (u'painting', painterName,),
+                                            }
 
-            invRegex = u'<p>[\r\n\s]+Inventory Number:[\r\n\s]+</p>[\r\n\s]+</div>[\r\n\s]+<div class="her-data-tbl-val">[\r\n\s]+<p>[\r\n\s]+(.*\d+)[\r\n\s]+</p>'
-            invMatch = re.search(invRegex, itemPageEnData)
+            invRegex = u'\<p\>[\r\n\s]+Inventory Number:[\r\n\s]+\<\/p\>[\r\n\s]+\<\/div\>[\r\n\s]+\<div class=\"her-data-tbl-val\"\>[\r\n\s]+\<p\>[\r\n\s]+(.*\d+[^\>]+)[\r\n\s]+\<\/p\>'
+            invMatch = re.search(invRegex, itemPageEn.text)
 
             if not invMatch:
                 pywikibot.output(u'No inventory number found! Skipping')
                 continue
             
-            metadata['id'] = invMatch.group(1)
+            metadata['id'] = invMatch.group(1).strip()
             metadata['idpid'] = u'P217'
 
-            dateDimRegex = u'var descriptionWoA = \'.*Date of creation: (.+), Dimension: ([^\s]+)x([^\s]+)\s?[sc]m\.?\';'
-            dateDimMatch = re.search(dateDimRegex, itemPageEnData)
-            if dateDimMatch:
-                metadata['inception'] = dateDimMatch.group(1)
-                metadata['height'] = dateDimMatch.group(2)
-                metadata['heightunitqid'] = u'Q174728'
-                metadata['width'] = dateDimMatch.group(2)
-                metadata['widthunitqid'] = u'Q174728'
+            materialregex = u'\<p\>[\r\n\s]+Material:[\r\n\s]+\<\/p\>[\r\n\s]+\<\/div\>[\r\n\s]+\<div class=\"her-data-tbl-val\"\>[\r\n\s]+\<a [^\>]+\>canvas\<\/a\>'
+            techniqueregex = u'\<p\>[\r\n\s]+Technique:[\r\n\s]+\<\/p\>[\r\n\s]+\<\/div\>[\r\n\s]+\<div class=\"her-data-tbl-val\"\>[\r\n\s]+\<p\>[\r\n\s]+oil[\r\n\s]+\<\/p\>'
 
+            materialMatch = re.search(materialregex, itemPageEn.text)
+            techniqueMatch = re.search(techniqueregex, itemPageEn.text)
+
+            if materialMatch and techniqueMatch:
+                metadata['medium'] = u'oil on canvas'
+
+            dateRegex = u'\<p\>[\r\n\s]+Date:[\r\n\s]+\<\/p\>[\r\n\s]+\<\/div\>[\r\n\s]+\<div class=\"her-data-tbl-val\"\>[\r\n\s]+\<a [^\>]+\>(\d\d\d\d)\<\/a\>'
+            dateMatch = re.search(dateRegex, itemPageEn.text)
+            if dateMatch:
+                metadata['inception'] = dateMatch.group(1)
+
+            dimRegex = u'\<p\>[\r\n\s]+Dimensions:[\r\n\s]+\<\/p\>[\r\n\s]+\<\/div\>[\r\n\s]+\<div class=\"her-data-tbl-val\"\>[\r\n\s]+\<p\>[\r\n\s]+([^\>]+)[\r\n\s]+\<\/p\>'
+            dimMatch = re.search(dimRegex, itemPageEn.text)
+
+            # Weird, sometimes they do width x height instead of height x width
+            if dimMatch:
+                dimtext = dimMatch.group(1).strip()
+                regex_2d = u'(?P<height>\d+(,\d+)?)\s*x\s*(?P<width>\d+(,\d+)?)\s*cm.*'
+                regex_3d = u'(?P<height>\d+(,\d+)?)\s*x\s*(?P<width>\d+(,\d+)?)\s*x\s*(?P<depth>\d+(,\d+)?)\s*cm.*'
+                match_2d = re.match(regex_2d, dimtext)
+                match_3d = re.match(regex_3d, dimtext)
+                if match_2d:
+                    metadata['heightcm'] = match_2d.group(u'height').replace(u',', u'.')
+                    metadata['widthcm'] = match_2d.group(u'width').replace(u',', u'.')
+                elif match_3d:
+                    metadata['heightcm'] = match_3d.group(u'height').replace(u',', u'.')
+                    metadata['widthcm'] = match_3d.group(u'width').replace(u',', u'.')
+                    metadata['depthcm'] = match_3d.group(u'depth').replace(u',', u'.')
 
             yield metadata
 
-            
-            
-            #print matchEn.group(1)
-            #print matchEn.group(2)
-            #print matchEn.group(3)
-            
-            
-
-        
-    '''        
-
-    paintinglisturl = u'https://raw.githubusercontent.com/artsmia/collection/master/departments/6.json'    
-    paintinglistPage = urllib2.urlopen(paintinglisturl)
-    paintinglistData = paintinglistPage.read()
-    paintinglistDataObject = json.loads(paintinglistData)
-
-    artists = {}
-    objectnames = {}
-    
-    # Open the artists and dump it in a dict id -> qid
-
-    with open('msk_artist_completed_2015-12_04.csv', 'rb') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            artists[row.get('creatorId')] = row.get('creatorWikidataPid').replace('http://www.wikidata.org/entity/', '').replace('http://www.wikidata.org/wiki/', '')
-    #print artists
-
-    # Open the types
-    # FIXME: Werkt nu alleen voor schilderijen!!!!
-    with open('MSK Gent AAT-Wikidata matching.csv', 'rb') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            objectnames[row.get('objectNameId')] = row.get('Wikidata Q')
-    #print objectnames
-
-    with open('MSK_import_wikidata_objects_16112015.csv', 'rb') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            metadata = {}
-
-
-            
-
-            metadata['title'] = { u'nl' : unicode(row.get('title'), u'utf-8') } # Hier iets met Nederlands doen
-            metadata['url'] = unicode(row.get('workPid'), u'utf-8')
-
-            name = unicode(row.get('creator'), u'utf-8')
-            # We need to normalize the name
-            if u',' in name:
-                (surname, sep, firstname) = name.partition(u',')
-            name = u'%s %s' % (firstname.strip(), surname.strip(),)
-            metadata['creatorname'] = name
-            
-            metadata['objectname'] = unicode(row.get('objectName'), u'utf-8')               
-
-            if metadata['creatorname'] and metadata['objectname']:
-                metadata['description'] = { u'nl' : u'%s van %s' % (metadata['objectname'], metadata['creatorname']) }
-                if metadata['objectname']==u'olieverfschilderij':
-                    metadata['description']['en'] = u'painting by %s' % (metadata['creatorname'],)
-                elif metadata['objectname']==u'beeldhouwwerk':
-                    metadata['description']['en'] = u'sculpture by %s' % (metadata['creatorname'],)
-                elif metadata['objectname']==u'aquarel':
-                    metadata['description']['en'] = u'watercolor painting by %s' % (metadata['creatorname'],)
-                    
-            if row.get('creatorId') in artists:
-                metadata['creatorqid'] = artists.get(row.get('creatorId'))
-
-            if row.get('objectNameId') in objectnames:
-                metadata['instanceofqid'] = objectnames.get(row.get('objectNameId'))
-
-            if row.get('dateIso8601'):
-                metadata['inception'] = unicode(row.get('dateIso8601'), u'utf-8')
-
-            # Start with only paintings
-            workwork = [u'olieverfschilderij',
-                        #u'beeldhouwwerk',
-                        #u'aquarel',
-                        ]
-            if metadata['objectname'] in workwork:
-                yield metadata
-            #else:
-            #    yield metadata
-    '''            
-        
 
 def main():
     dictGen = getHermitageGenerator()
@@ -221,8 +141,6 @@ def main():
 
     artDataBot = artdatabot.ArtDataBot(dictGen, create=True)
     artDataBot.run()
-    
-    
 
 if __name__ == "__main__":
     main()
