8fa422d0a7f315ffb6eae472d54d5e451a335fc1
multichill <maarten@mdammers.nl>
2017-01-27 16:34:25 +0100

    collection and venue

--- bot/wikidata/art_uk_works_link.py
+++ bot/wikidata/art_uk_works_link.py
@@ -21,65 +21,81 @@ import requests
 import re
 import time
 
-def getVenue(collectionid):
+def getCollectionVenue(collectionid):
     repo = pywikibot.Site().data_repository()
     item = pywikibot.ItemPage(repo, title=collectionid)
     data = item.get()
 
     claims = data.get('claims')
+    if u'P1751' in claims:
+        result = u'collection:%s' % (claims.get(u'P1751')[0].getTarget(), )
+        return result
+
     if u'P1602' in claims:
-        result = claims.get(u'P1602')[0].getTarget()
+        result = u'venue:%s' % (claims.get(u'P1602')[0].getTarget(), )
         return result
     else:
         pywikibot.output(u'No venue claim found')
 
-def getArtUKWorkGenerator(venuid):
+def getArtUKWorkGenerator(collectionVenueId, artukcache):
     """
     Generator to return KID paintings
     """
     i = 1
     load_more = True
-    basesearchurl = u'http://artuk.org/discover/artworks/search/venue:%s/page/%s?_ajax=1'
+    basesearchurl = u'http://artuk.org/discover/artworks/search/%s/page/%s?_ajax=1'
     while load_more:
-        searchurl = basesearchurl % (venuid, i)
+        searchurl = basesearchurl % (collectionVenueId, i)
         searchpage = requests.get(searchurl, headers={'X-Requested-With' : 'XMLHttpRequest',} )
         searchjson = searchpage.json()
         load_more = searchjson.get('load_more')
         i = i + 1
         searchtext = searchjson.get('html')
 
-        idregex = u'\"http\:\/\/artuk\.org\/discover\/artworks\/([^\/]+)/search\/venue:[^\"]+/page/\d+\"'
+        idregex = u'\"http\:\/\/artuk\.org\/discover\/artworks\/([^\/]+)/search\/(collection|venue):[^\"]+/page/\d+\"'
         for match in re.finditer(idregex, searchtext):
             artukid = match.group(1)
-            url = u'http://artuk.org/discover/artworks/%s' % (artukid,)
-            itempage = requests.get(url)
-            invregex = u'\<li\>[\s\t\r\n]+\<h5\>Accession number\<\/h5\>[\s\t\r\n]+\<p\>([^\<]+)\<\/p\>[\s\t\r\n]+\<\/li\>'
-            invmatch = re.search(invregex, itempage.text)
-            if invmatch:
-                invid = invmatch.group(1).strip()
-                metadata = {u'invnum' : invid,
-                            u'artukid' : artukid,
-                            u'url' : url}
-                yield metadata
+            # Only load the page if we don't already have it.
+            if not artukid in artukcache:
+                url = u'http://artuk.org/discover/artworks/%s' % (artukid,)
+                itempage = requests.get(url)
+                invregex = u'\<li\>[\s\t\r\n]+\<h5\>Accession number\<\/h5\>[\s\t\r\n]+\<p\>([^\<]+)\<\/p\>[\s\t\r\n]+\<\/li\>'
+                invmatch = re.search(invregex, itempage.text)
+                if invmatch:
+                    invid = invmatch.group(1).strip()
+                    metadata = {u'invnum' : invid,
+                                u'artukid' : artukid,
+                                u'url' : url}
+                    yield metadata
 
-def fillCache(collectionqid):
+def fillCaches(collectionqid):
     '''
     Build an ID cache so we can quickly look up the id's for property.
-    Only return items for which we don't already have the Art UK artwork ID (P1679) link
+    Only return items in this ID cache for which we don't already have the Art UK artwork ID (P1679) link
+
+    Build a second art uk -> Qid cache for items we don't have to process
     '''
-    result = {}
+    invcache = {}
+    artukcache = {}
     sq = pywikibot.data.sparql.SparqlQuery()
 
     # FIXME: Do something with the collection qualifier
-    query = u'SELECT ?item ?id WHERE { ?item wdt:P195 wd:%s . ?item wdt:P217 ?id . MINUS { ?item wdt:P1679 [] } } ' % (collectionqid,)
+    query = u'SELECT ?item ?inv ?artukid WHERE { ?item wdt:P195 wd:%s . ?item wdt:P217 ?inv . OPTIONAL { ?item wdt:P1679 ?artukid } } ' % (collectionqid,)
     sq = pywikibot.data.sparql.SparqlQuery()
     queryresult = sq.select(query)
 
     for resultitem in queryresult:
         qid = resultitem.get('item').replace(u'http://www.wikidata.org/entity/', u'')
-        result[resultitem.get('id')] = qid
-    pywikibot.output(u'The query "%s" returned %s items without an ART UK work link' % (query, len(result)))
-    return result
+        if resultitem.get('artukid'):
+            artukcache[resultitem.get('artukid')] = qid
+        else:
+            invcache[resultitem.get('inv')] = qid
+    pywikibot.output(u'The query "%s" returned %s with and %s items without an ART UK work link' % (query,
+                                                                                                    len(artukcache),
+                                                                                                    len(invcache)))
+    return (invcache,artukcache)
+
+
 
 def main(*args):
     collectionid = None
@@ -96,19 +112,19 @@ def main(*args):
         pywikibot.output(u'Usage: python pwb.py /path/to/art_uk_works_link.py -collectionid:Q180788')
         return
 
-    venueid = getVenue(collectionid)
-    if not venueid:
-        pywikibot.output(u'No valid venue found')
+    collectionVenueId = getCollectionVenue(collectionid)
+    if not collectionVenueId:
+        pywikibot.output(u'No valid collection or venue found')
         return
 
-    cache = fillCache(collectionid)
-    dictGen = getArtUKWorkGenerator(venueid)
+    (invcache,artukcache) = fillCaches(collectionid)
+    dictGen = getArtUKWorkGenerator(collectionVenueId, artukcache)
     repo = pywikibot.Site().data_repository()
 
     for work in dictGen:
         print(work)
-        if work.get(u'invnum') in cache:
-            artworkItemTitle = cache.get(work.get(u'invnum'))
+        if work.get(u'invnum') in invcache:
+            artworkItemTitle = invcache.get(work.get(u'invnum'))
             pywikibot.output(u'Found an artwork to add a link to: %s' % (artworkItemTitle,))
             artworkItem = pywikibot.ItemPage(repo, title=artworkItemTitle)
             data = artworkItem.get()
