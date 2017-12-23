c1665dc9472691c571d3d06747268ce151a86c3c
multichill <maarten@mdammers.nl>
2017-12-23 22:26:17 +0100

    Update and clean up

--- bot/wikidata/yale_import.py
+++ bot/wikidata/yale_import.py
@@ -12,60 +12,29 @@ Use artdatabot to upload it to Wikidata
 import artdatabot
 import pywikibot
 import requests
-import urllib2
 import re
 import HTMLParser
-import xml.etree.ElementTree as ET
-
 
 def getYaleGenerator():
     """
     Generator to return Yale paintings
     
-    searchurl= u''
-    sparqlurl = u'http://collection.britishart.yale.edu/openrdf-sesame/repositories/ycba?query=PREFIX+owl%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23%3E%0D%0APREFIX+rdf%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0D%0APREFIX+rdfs%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0D%0APREFIX+dc%3A+%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Felements%2F1.1%2F%3E%0D%0APREFIX+crm%3A+%3Chttp%3A%2F%2Ferlangen-crm.org%2Fcurrent%2F%3E%0D%0APREFIX+foaf%3A+%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0D%0APREFIX+skos%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2004%2F02%2Fskos%2Fcore%23%3E%0D%0APREFIX+xsd%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23%3E%0D%0APREFIX+lido%3A+%3Chttp%3A%2F%2Fwww.lido-schema.org%2F%3E%0D%0APREFIX+bibo%3A+%3Chttp%3A%2F%2Fpurl.org%2Fontology%2Fbibo%2F%3E%0D%0APREFIX+dcterms%3A+%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Fterms%2F%3E%0D%0APREFIX+ycba%3A+%3Chttp%3A%2F%2Fcollection.britishart.yale.edu%2Fid%2F%3E%0D%0APREFIX+ycba_ont%3A+%3Chttp%3A%2F%2Fcollection.britishart.yale.edu%2Fid%2Fontology%2F%3E%0D%0APREFIX+ycba_aat%3A+%3Chttp%3A%2F%2Fcollection.britishart.yale.edu%2Fid%2Faat%2F%3E%0D%0APREFIX+ycba_ulan%3A+%3Chttp%3A%2F%2Fcollection.britishart.yale.edu%2Fid%2Fulan%2F%3E%0D%0APREFIX+ycba_tgn%3A+%3Chttp%3A%2F%2Fcollection.britishart.yale.edu%2Fid%2Ftgn%2F%3E%0D%0APREFIX+aat%3A+%3Chttp%3A%2F%2Fcollection.getty.edu%2Fid%2Faat%2F%3E%0D%0APREFIX+ulan%3A+%3Chttp%3A%2F%2Fcollection.getty.edu%2Fid%2Fulan%2F%3E%0D%0APREFIX+tgn%3A+%3Chttp%3A%2F%2Fcollection.getty.edu%2Fid%2Ftgn%2F%3E%0D%0ASELECT+DISTINCT+*+WHERE+%7B%0D%0A%09%3Frecord+crm%3AP2_has_type+%3Chttp%3A%2F%2Fvocab.getty.edu%2Faat%2F300033618%3E+.%0D%0A%7D&output=xml'
-    sparqlpage = requests.get(sparqlurl)
-    sparqlpage.encoding='utf-8'
-    pywikibot.output(sparqlpage.text)
-
-    baseurl = u'http://collections.britishart.yale.edu/vufind/Record/%s'
-    basexmlurl = u'http://collections.britishart.yale.edu/oaicatmuseum/OAIHandler?verb=GetRecord&identifier=oai:tms.ycba.yale.edu:%s&metadataPrefix=lido'
-
-    # BOOO HOOO, for some reason I get binary encoded crap. Let's just extract the id's so we can continue
-    # Only seem to be getting about 800 id's, would have expected about 4000
-    for match in re.finditer(u'(\d+)', sparqlpage.text):
-        print match.group(1)
-        if match.group(1)==u'0':
-            continue
-        url = baseurl % (match.group(1),)
-        xmlurl = basexmlurl % (match.group(1),)
-
-        xmlpage = requests.get(xmlurl)
-        #xmlpage.encoding='utf-8'
-        #pywikibot.output(xmlpage.text)
-        
-
-        root = ET.fromstring(xmlpage.text.encode(u'utf-8'))
-        print root.keys()
-        print root.items()
-        for bla in root.iter():
-            print bla
-        
-        ET.dump(root.find('OAI-PMH'))#.get('GetRecord').get('record').get('metadata'))
     """
 
     # 1 - 22
-    searchBaseUrl = u'http://collections.britishart.yale.edu/vufind/Search/Results?join=AND&bool0[]=AND&lookfor0[]=%%22Paintings+and+Sculpture%%22&type0[]=collection&bool1[]=AND&lookfor1[]=Painting&type1[]=type_facet&page=%s&view=grid'
-    baseUrl = u'https://www.hermitagemuseum.org%s'
+    #searchBaseUrl = u'http://collections.britishart.yale.edu/vufind/Search/Results?join=AND&bool0[]=AND&lookfor0[]=%%22Paintings+and+Sculpture%%22&type0[]=collection&bool1[]=AND&lookfor1[]=Painting&type1[]=type_facet&page=%s&view=grid'
+    searchBaseUrl = u'http://collections.britishart.yale.edu/vufind/Search/Results?lookfor=&type=AllFields&filter[]=object_name_facet%%3A%%22painting%%22&page=%s&view=grid'
     htmlparser = HTMLParser.HTMLParser()
 
-    foundit=False
+    session = requests.Session()
+
+    foundit=True
 
     for i in range(1, 22):
         searchUrl = searchBaseUrl % (i,)
         print searchUrl
-        searchPage = urllib2.urlopen(searchUrl)
-        searchPageData = searchPage.read()
+        searchPage = session.get(searchUrl)
+        searchPageData = searchPage.text
 
         searchRegex = u'(http://collections.britishart.yale.edu/vufind/Record/\d+)'
         matches = re.finditer(searchRegex, searchPageData)
@@ -92,8 +61,8 @@ def getYaleGenerator():
             
             metadata['url'] = url
 
-            itemPage = urllib2.urlopen(url)
-            itemPageData = unicode(itemPage.read(), u'utf-8')
+            itemPage = session.get(url)
+            itemPageData = itemPage.text # unicode(itemPage.read(), u'utf-8')
             
             #print itemPageEnData
             titleRegex = u'<th id\="titleHeaders">Title\s*</th>[\r\n\t\s]+<td id="dataField">[\r\n\t\s]+([^<]+)[\r\n\t\s]+</td>'
@@ -122,9 +91,7 @@ def getYaleGenerator():
                                         u'en' : u'%s by %s' % (u'painting', metadata.get('creatorname'),),
                                         }
 
-            #pywikibot.output(metadata.get('description'))
-
-            invRegex = u'<th id="titleHeaders">Accession Number[\r\n\t\s]+</th>[\r\n\t\s]+<td id="dataField">[\r\n\t\s]+<span title="Object ID:[\r\n\t\s]+\d+">[\r\n\t\s]+([^\r\n\t\s]+)[\r\n\t\s]+</span>[\r\n\t\s]+</td>'
+            invRegex = u'<th id=\"titleHeaders\">Accession Number[\r\n\t\s]+</th>[\r\n\t\s]+<td id=\"dataField\">[\r\n\t\s]+<span title="Object ID:[\r\n\t\s]+\d+">[\r\n\t\s]+([^\r\n\t\s]+)[\r\n\t\s]+</span>[\r\n\t\s]+</td>'
             invMatch = re.search(invRegex, itemPageData)
 
             if not invMatch:
@@ -134,16 +101,39 @@ def getYaleGenerator():
             metadata['id'] = invMatch.group(1)
             metadata['idpid'] = u'P217'
 
-
-            dateRegex = u'<th id="titleHeaders">Date[\r\n\t\s]+</th>[\r\n\t\s]+<td id="dataField">[\r\n\t\s]+([^<]+)<br>[\r\n\t\s]+</td>'
+            dateRegex = u'<th id=\"titleHeaders\">Date[\r\n\t\s]+</th>[\r\n\t\s]+<td id=\"dataField\">[\r\n\t\s]+([^<]+)<br>[\r\n\t\s]+</td>'
             dateMatch = re.search(dateRegex, itemPageData)
 
             if dateMatch:
                 metadata['inception'] = dateMatch.group(1)
 
+            # Data not available
+            # record.get('acquisition')
+
+            mediumRegex = u'<th id=\"titleHeaders\">Medium[\r\n\t\s]+</th>[\r\n\t\s]+<td id=\"dataField\">[\r\n\t\s]+([^<]+)[\r\n\t\s]+</td>'
+            mediumMatch = re.search(mediumRegex, itemPageData)
+
+            if mediumMatch and mediumMatch.group(1).strip()==u'Oil on canvas':
+                metadata['medium'] = u'oil on canvas'
+
+            dimensionRegex = u'<th id=\"titleHeaders\">Dimensions[\r\n\t\s]+</th>[\r\n\t\s]+<td id=\"dataField\">[\r\n\t\s]+([^<]+)[\r\n\t\s]+</td>'
+            dimensionMatch = re.search(dimensionRegex, itemPageData)
+
+            if dimensionMatch:
+                dimensiontext = dimensionMatch.group(1).strip()
+                regex_2d = u'.*\((?P<height>\d+(\.\d+)?) (x|×) (?P<width>\d+(\.\d+)?) cm\)'
+                regex_3d = u'.*\((?P<height>\d+(\.\d+)?) (x|×) (?P<width>\d+(\.\d+)?) (x|×) (?P<depth>\d+(\.\d+)?) cm\)'
+                match_2d = re.match(regex_2d, dimensiontext)
+                match_3d = re.match(regex_3d, dimensiontext)
+                if match_2d:
+                    metadata['heightcm'] = match_2d.group(u'height')
+                    metadata['widthcm'] = match_2d.group(u'width')
+                elif match_3d:
+                    metadata['heightcm'] = match_3d.group(u'height')
+                    metadata['widthcm'] = match_3d.group(u'width')
+                    metadata['depthcm'] = match_3d.group(u'depth')
             yield metadata
 
-        
 
 def main():
     dictGen = getYaleGenerator()
@@ -153,8 +143,6 @@ def main():
 
     artDataBot = artdatabot.ArtDataBot(dictGen, create=True)
     artDataBot.run()
-    
-    
 
 if __name__ == "__main__":
     main()
