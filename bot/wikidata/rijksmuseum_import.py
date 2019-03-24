9b9b5dfbe413bab73623f91654c3b338dbe58756
multichill <maarten@mdammers.nl>
2019-03-24 18:40:47 +0100

    Date improvements

--- bot/wikidata/rijksmuseum_import.py
+++ bot/wikidata/rijksmuseum_import.py
@@ -12,6 +12,7 @@ Manual at http://rijksmuseum.github.io/
 import artdatabot
 import pywikibot
 import requests
+import re
 
 def getRijksmuseumGenerator():
     """
@@ -85,17 +86,27 @@ def getRijksmuseumGenerator():
 
             # Set the inception only if start only start or if start and end are the same
             if record.get('dating') and \
-                record.get('dating').get('year'):
-                proddate = record.get('dating').get('year')
+                record.get('dating').get('sortingDate'):
+                proddate = record.get('dating').get('sortingDate')
                 if proddate == record.get('dating').get('yearEarly') == record.get('dating').get('yearLate'):
                     metadata['inception'] = proddate
+                else:
+                    if 1000 < record.get('dating').get('yearEarly') < 2500 and \
+                                            1000 < record.get('dating').get('yearLate') < 2500:
+                        metadata['inceptionstart'] = record.get('dating').get('yearEarly')
+                        metadata['inceptionend'] = record.get('dating').get('yearLate')
 
             # Provenance.
             # TODO: Test and update artdatabot to actually use it.
             if record.get('acquisition'):
                 if record.get('acquisition').get('date'):
                     # This is not going to work yet. Artdatabot only understands years
-                    metadata[u'acquisitiondate'] = u'+%s' % (record.get('acquisition').get('date'),)
+                    acquisitiondateRegex = u'^(\d\d\d\d)-01-01T00:00:00Z$'
+                    acquisitiondateMatch = re.match(acquisitiondateRegex, record.get('acquisition').get('date'))
+                    if acquisitiondateMatch:
+                        metadata['acquisitiondate'] = acquisitiondateMatch.group(1)
+                    else:
+                        metadata[u'acquisitiondate'] = u'+%s' % (record.get('acquisition').get('date'),)
                 if record.get('acquisition').get('creditLine'):
                     if record.get('acquisition').get('creditLine')==u'Bruikleen van de gemeente Amsterdam (legaat A. van der Hoop)':
                         metadata['extracollectionqid'] = u'Q19750488'
