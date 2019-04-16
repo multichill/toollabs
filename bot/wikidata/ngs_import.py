67627201de12f763bff961497e39e1f738b11776
multichill <maarten@mdammers.nl>
2019-04-16 13:07:51 +0200

    NGS update
    * Site changed so had to update all regex
    * Better date handling

--- bot/wikidata/ngs_import.py
+++ bot/wikidata/ngs_import.py
@@ -14,21 +14,22 @@ import pywikibot
 import requests
 import re
 import time
-import HTMLParser
+from html.parser import HTMLParser
 
 def getNGSGenerator():
     """
     Generator to return National Galleries of Scotland paintings
     """
     basesearchurl = u'https://art.nationalgalleries.org/search?object_types[29864]=29864&page=%s'
-    htmlparser = HTMLParser.HTMLParser()
+    htmlparser = HTMLParser()
 
+    # Number per page seems to have changed, but that doesn't crash the bot
     for i in range (0,125):
         urls = []
         searchurl = basesearchurl % (i,)
-        print searchurl
+        print (searchurl)
         searchPage = requests.get(searchurl)
-        urlregex = u'\<a href\=\"(\/art-and-artists\/\d+\/[^\?]+)\?object_types\[29864\]=2986[^\"]+\"' # ?
+        urlregex = u'\<a href\=\"(\/art-and-artists\/\d+\/[^\?]+)\?object_types%5B29864%5D=2986[^\"]+\"' # ?
         matches = re.finditer(urlregex, searchPage.text)
         for match in matches:
             # To remove duplicates
@@ -39,7 +40,7 @@ def getNGSGenerator():
         for url in urls:
             metadata = {}
 
-            print url
+            print (url)
 
             itempage = requests.get(url)
             metadata['url'] = url
@@ -50,7 +51,7 @@ def getNGSGenerator():
             #No need to check, I'm actually searching for paintings.
             metadata['instanceofqid'] = u'Q3305213'
 
-            invregex = u'\<li class\=\"ngs-mimsy-data__item\"\>\s*\<span class\=\"ngs-mimsy-data__item-label\"\>accession number\:</span\>\s*\<span class\=\"ngs-mimsy-data__item-values\"\>([^\<]+)\<\/span\>\<\/li\>'
+            invregex = u'\<li class\=\"ngs-mimsy-data__item\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-label\"\>accession number\:\<\/div\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-values\"\>[\r\n\t\s]*([^\<]+)[\r\n\t\s]*\<\/div\>[\r\n\t\s]*\<\/li\>'
             invmatch = re.search(invregex, itempage.text)
 
             if not invmatch:
@@ -70,7 +71,7 @@ def getNGSGenerator():
             else:
                 metadata['locationqid'] = u'Q2051997'
 
-            titleregex = u'\<li class\=\"ngs-mimsy-data__item\"\>\s*\<span class\=\"ngs-mimsy-data__item-label\"\>title\:</span\>\s*\<span class\=\"ngs-mimsy-data__item-values\"\>([^\<]+)\<\/span\>\<\/li\>'
+            titleregex = u'\<li class\=\"ngs-mimsy-data__item\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-label\"\>title\:</div\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-values\"\>[\r\n\t\s]*([^\<]+)[\r\n\t\s]*\<\/div\>[\r\n\t\s]*\<\/li\>'
             titlematch = re.search(titleregex, itempage.text)
             title = htmlparser.unescape(titlematch.group(1).strip())
             #if not titlematch:
@@ -82,7 +83,7 @@ def getNGSGenerator():
             metadata['title'] = { u'en' : title,
                                   }
 
-            creatorregex = u'\<li class\=\"ngs-mimsy-data__item\"\>\s*\<span class\=\"ngs-mimsy-data__item-label\"\>artists?\:</span\>\s*\<span class\=\"ngs-mimsy-data__item-values\"\>(\<a href\=\"[^\"]+\" title=\"[^\"]+\"\>)?(?P<creator>[^\<]+)\<\/'
+            creatorregex = u'\<li class\=\"ngs-mimsy-data__item\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-label\"\>artists?\:</div\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-values\"\>[\r\n\t\s]*(\<a href\=\"[^\"]+\" title=\"[^\>]+\"\>)?(?P<creator>[^\<]+)\<\/'
             creatormatch = re.search(creatorregex, itempage.text)
             name = htmlparser.unescape(creatormatch.group(u'creator').strip())
             if u',' in name:
@@ -100,26 +101,57 @@ def getNGSGenerator():
                                             u'en' : u'%s by %s' % (u'painting', name,),
                                             }
 
-            acquisitiondateregex = u'\<li class\=\"ngs-mimsy-data__item\"\>\s*\<span class\=\"ngs-mimsy-data__item-label\"\>credit line\:</span\>\s*\<span class\=\"ngs-mimsy-data__item-values\"\>[^\<]+(\d\d\d\d)\<\/span\>\<\/li\>'
+            acquisitiondateregex = u'\<li class\=\"ngs-mimsy-data__item\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-label\"\>credit line\:</div\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-values\"\>[\r\n\t\s]*[^\<]+(\d\d\d\d)[\r\n\t\s]*\<\/div\>[\r\n\t\s]*\<\/li\>'
             acquisitiondatematch = re.search(acquisitiondateregex, itempage.text)
             if acquisitiondatematch:
                 metadata['acquisitiondate'] = acquisitiondatematch.group(1)
 
-            inceptionregex = u'\<li class\=\"ngs-mimsy-data__item\"\>\s*\<span class\=\"ngs-mimsy-data__item-label\"\>date created\:</span\>\s*\<span class\=\"ngs-mimsy-data__item-values\"\>(\d\d\d\d)\<\/span\>\<\/li\>'
-            inceptionmatch = re.search(inceptionregex, itempage.text)
-            if inceptionmatch:
-                metadata['inception'] = inceptionmatch.group(1)
+            dateregex = u'\<li class\=\"ngs-mimsy-data__item\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-label\"\>date created\:</div\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-values\"\>[\r\n\t\s]*(\d\d\d\d)[\r\n\t\s]*\<\/div\>[\r\n\t\s]*\<\/li\>'
+            datedregex = u'\<li class\=\"ngs-mimsy-data__item\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-label\"\>date created\:</div\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-values\"\>[\r\n\t\s]*Dated\s*(\d\d\d\d)[\r\n\t\s]*\<\/div\>[\r\n\t\s]*\<\/li\>'
+            datecircaregex = u'\<li class\=\"ngs-mimsy-data__item\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-label\"\>date created\:</div\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-values\"\>[\r\n\t\s]*[aA]bout\s*(\d\d\d\d)[\r\n\t\s]*\<\/div\>[\r\n\t\s]*\<\/li\>'
+            periodregex = u'\<li class\=\"ngs-mimsy-data__item\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-label\"\>date created\:</div\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-values\"\>[\r\n\t\s]*(\d\d\d\d)\s*-\s*(\d\d\d\d)[\r\n\t\s]*\<\/div\>[\r\n\t\s]*\<\/li\>'
+            shortperiodregex = u'\<li class\=\"ngs-mimsy-data__item\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-label\"\>date created\:</div\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-values\"\>[\r\n\t\s]*(\d\d)(\d\d)\s*-\s*(\d\d)[\r\n\t\s]*\<\/div\>[\r\n\t\s]*\<\/li\>'
+            circaperiodregex = u'\<li class\=\"ngs-mimsy-data__item\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-label\"\>date created\:</div\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-values\"\>[\r\n\t\s]*[aA]bout\s*(\d\d\d\d)\s*-\s*(\d\d\d\d)[\r\n\t\s]*\<\/div\>[\r\n\t\s]*\<\/li\>'
+            otherdateregex = u'\<li class\=\"ngs-mimsy-data__item\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-label\"\>date created\:</div\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-values\"\>[\r\n\t\s]*([^\<]+)[\r\n\t\s]*\<\/div\>[\r\n\t\s]*\<\/li\>'
 
-            mediumregex = u'\<li class\=\"ngs-mimsy-data__item\"\>\s*\<span class\=\"ngs-mimsy-data__item-label\"\>medium\:</span\>\s*\<span class\=\"ngs-mimsy-data__item-values\"\>([^\<]+)\<\/span\>\<\/li\>'
+            datematch = re.search(dateregex, itempage.text)
+            datedmatch = re.search(datedregex, itempage.text)
+            datecircamatch = re.search(datecircaregex, itempage.text)
+            periodmatch = re.search(periodregex, itempage.text)
+            shortperiodmatch = re.search(shortperiodregex, itempage.text)
+            circaperiodmatch = re.search(circaperiodregex, itempage.text)
+            otherdatematch = re.search(otherdateregex, itempage.text)
+
+            if datematch:
+                metadata['inception'] = datematch.group(1)
+            elif datedmatch:
+                metadata['inception'] = datedmatch.group(1)
+            elif datecircamatch:
+                metadata['inception'] = datecircamatch.group(1)
+                metadata['inceptioncirca'] = True
+            elif periodmatch:
+                metadata['inceptionstart'] = int(periodmatch.group(1),)
+                metadata['inceptionend'] = int(periodmatch.group(2),)
+            elif shortperiodmatch:
+                metadata['inceptionstart'] = int(u'%s%s' % (shortperiodmatch.group(1),shortperiodmatch.group(2)))
+                metadata['inceptionend'] = int(u'%s%s' % (shortperiodmatch.group(1),shortperiodmatch.group(3)))
+            elif circaperiodmatch:
+                metadata['inceptionstart'] = int(circaperiodmatch.group(1),)
+                metadata['inceptionend'] = int(circaperiodmatch.group(2),)
+                metadata['inceptioncirca'] = True
+            elif otherdatematch:
+                print (u'Could not parse date: "%s"' % (otherdatematch.group(1).strip(),))
+
+            mediumregex = u'\<li class\=\"ngs-mimsy-data__item\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-label\"\>materials\:\<\/div\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-values\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-value\"\>([^\<]+)\<\/div\>[\r\n\t\s]*\<\/div\>[\r\n\t\s]*\<\/li\>'
 
             mediummatch = re.search(mediumregex, itempage.text)
             if mediummatch and mediummatch.group(1) == u'Oil on canvas':
                 metadata['medium'] = u'oil on canvas'
 
-            measurementsregex = u'\<li class\=\"ngs-mimsy-data__item\"\>\s*\<span class\=\"ngs-mimsy-data__item-label\"\>measurements\:</span\>\s*\<span class\=\"ngs-mimsy-data__item-values\"\>([^\<]+)\<\/span\>\<\/li\>'
+            measurementsregex = u'\<li class\=\"ngs-mimsy-data__item\"\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-label\"\>measurements\:</div\>[\r\n\t\s]*\<div class\=\"ngs-mimsy-data__item-values\"\>[\r\n\t\s]*([^\<]+)[\r\n\t\s]*\<\/div\>[\r\n\t\s]*\<\/li\>'
             measurementsmatch = re.search(measurementsregex, itempage.text)
             if measurementsmatch:
-                measurementstext = measurementsmatch.group(1)
+                measurementstext = measurementsmatch.group(1).strip()
                 regex_2d = u'(?P<height>\d+(\.\d+)?) x (?P<width>\d+(\.\d+)?) cm.*'
                 regex_3d = u'(?P<height>\d+(\.\d+)?) x (?P<width>\d+(\.\d+)?) x (?P<depth>\d+(\.\d+)?) cm.*'
                 match_2d = re.match(regex_2d, measurementstext)
@@ -139,7 +171,7 @@ def main():
     dictGen = getNGSGenerator()
 
     #for painting in dictGen:
-    #    print painting
+    #    print (painting)
 
     artDataBot = artdatabot.ArtDataBot(dictGen, create=True)
     artDataBot.run()
