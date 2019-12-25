342996f6153ff4cf15640ad6fdc15ec19fd6b05c
multichill <maarten@mdammers.nl>
2019-12-25 12:54:28 +0100

    Add genre, where it was made and more dates to National Trust

--- bot/wikidata/national_trust_import.py
+++ bot/wikidata/national_trust_import.py
@@ -22,7 +22,7 @@ except ImportError:
 
 def getNTGenerator():
     """
-    Generator to return Hungarian National Trust paintings
+    Generator to return National Trust paintings
 
     Search has a max of 250 pages, so that's 5*5*250=6250 of the 12,472 paintings.
     So need to try the different ways to get all of them.
@@ -30,7 +30,8 @@ def getNTGenerator():
     """
     htmlparser = HTMLParser()
     locations = nationalTrustLocationsOnWikidata()
-    missedlocations = {}
+    missedlocations = {} # Where it is now
+    missedplaces = {} # Where it was made
     baseSearchUrl = u'http://www.nationaltrustcollections.org.uk/results?Categories=7456ee20fffffe0702132e04e5764fd3&Sort=collection&Page=%s'
 
     for i in range(1, 250):
@@ -135,24 +136,36 @@ def getNTGenerator():
             metadata['description'] = { u'nl' : u'%s van %s' % (u'schilderij', metadata.get('creatorname'),),
                                         u'en' : u'%s by %s' % (u'painting', metadata.get('creatorname'),),
                                         u'de' : u'%s von %s' % (u'Gemälde', metadata.get('creatorname'),),
+                                        u'fr' : u'%s de %s' % (u'peinture', metadata.get('creatorname'), ),
                                         }
 
             # Only match on years
-            dateRegex = u'\<h4\>Date\<\/h4\>[\r\n\t\s]*\<p\>\s*(\d\d\d\d)\s*(\(signed and dated\))?\<\/p\>'
-            circadateRegex = u'\<h4\>Date\<\/h4\>[\r\n\t\s]*\<p\>\s*circa (\d\d\d\d)\s*\<\/p\>'
-            perioddateRegex = u'\<h4\>Date\<\/h4\>[\r\n\t\s]*\<p\>\s*(\d\d\d\d)\s*-\s*(\d\d\d\d)\s*\<\/p\>'
+            dateregex = u'\<h4\>Date\<\/h4\>[\r\n\t\s]*\<p\>\s*(\d\d\d\d)\s*(\(signed and dated\))?\<\/p\>'
+            circadateregex = u'\<h4\>Date\<\/h4\>[\r\n\t\s]*\<p\>\s*circa (\d\d\d\d)\s*\<\/p\>'
+            periodregex = u'\<h4\>Date\<\/h4\>[\r\n\t\s]*\<p\>\s*(\d\d\d\d)\s*-\s*(\d\d\d\d)\s*\<\/p\>'
+            circaperiodegex = u'\<h4\>Date\<\/h4\>[\r\n\t\s]*\<p\>\s*circa\s*(\d\d\d\d)\s*-\s*(\d\d\d\d)\s*\<\/p\>'
+            otherdateregex = u'\<h4\>Date\<\/h4\>[\r\n\t\s]*\<p\>\s*([^\<]+)\s*\<\/p\>'
 
-            dateMatch = re.search(dateRegex, itemPageData)
-            circadateMatch = re.search(circadateRegex, itemPageData)
-            perioddateMatch = re.search(perioddateRegex, itemPageData)
-            if dateMatch:
-                metadata['inception'] = htmlparser.unescape(dateMatch.group(1))
-            elif circadateMatch:
-                metadata['inception'] = htmlparser.unescape(circadateMatch.group(1))
+            datematch = re.search(dateregex, itemPageData)
+            circadatematch = re.search(circadateregex, itemPageData)
+            periodmatch = re.search(periodregex, itemPageData)
+            circaperiodmatch = re.search(circaperiodegex, itemPageData)
+            otherdatematch = re.search(otherdateregex, itemPageData)
+
+            if datematch:
+                metadata['inception'] = htmlparser.unescape(datematch.group(1))
+            elif circadatematch:
+                metadata['inception'] = htmlparser.unescape(circadatematch.group(1))
                 metadata['inceptioncirca'] = True
-            elif perioddateMatch:
-                metadata['inceptionstart'] = int(perioddateMatch.group(1),)
-                metadata['inceptionend'] = int(perioddateMatch.group(2),)
+            elif periodmatch:
+                metadata['inceptionstart'] = int(periodmatch.group(1),)
+                metadata['inceptionend'] = int(periodmatch.group(2),)
+            elif circaperiodmatch:
+                metadata['inceptionstart'] = int(circaperiodmatch.group(1))
+                metadata['inceptionend'] = int(circaperiodmatch.group(2))
+                metadata['inceptioncirca'] = True
+            elif otherdatematch:
+                print (u'Could not parse date: "%s"' % (otherdatematch.group(1),))
 
             # acquisitiondate not available
             # acquisitiondateRegex = u'\<em\>Acknowledgement\<\/em\>\:\s*.+(\d\d\d\d)[\r\n\t\s]*\<br\>'
@@ -183,6 +196,77 @@ def getNTGenerator():
                     metadata['widthcm'] = u'%s' % (float(match_3d.group(u'width'))/10, )
                     metadata['depthcm'] = u'%s' % (float(match_3d.group(u'depth'))/10, )
 
+            # Add genre
+            genres = { u'Animals' : u'Q16875712',
+                       u'Scenes of everyday life (genre)' : u'Q1047337',
+                       u'History' : u'Q742333',
+                       u'Landscape' : u'Q191163',
+                       u'Portrait' : u'Q134307',
+                       u'Religion' : u'Q2864737',
+                       u'Townscape' : u'Q1935974',
+                       u'Still Life' : u'Q170571',
+                       u'Seascape' : u'Q158607',
+                       }
+
+            genresubjectregex = u'\<a href\=\"\/results\?Subject\=([^\"]+)\"\>([^\<]+)\<\/a\>'
+            genresubjectmatches = re.finditer(genresubjectregex, itemPageData)
+
+            if genresubjectmatches:
+                for genresubjectmatch in genresubjectmatches:
+                    if genresubjectmatch.group(2) in genres:
+                        metadata[u'genreqid'] = genres.get(genresubjectmatch.group(2))
+                        break
+                    else:
+                        print (u'Genre %s not found' % (genresubjectmatch.group(2),))
+
+            if not metadata.get(u'genreqid'):
+                genresummaryregex = u'\<h2\>Summary\<\/h2\>[\r\n\t\s]*\<p style\=\"white-space\: pre-line\"\>[^\<]*(Portrait of|three quarter length Portrait of|Framed portrait of|half-length portrait)[^\<]*\<\/p\>'
+                genresummarymatch = re.search(genresummaryregex, itemPageData)
+                if genresummarymatch:
+                    metadata[u'genreqid'] = u'Q134307' # Portrait
+                    print (u'Summary based portrait found')
+
+            # Add place made
+            places = { u'Amsterdam' : u'Q727',
+                       u'Bologna' : u'Q1891',
+                       u'Cambridge' : u'Q350',
+                       u'China' : u'Q29520',
+                       u'Denmark' : u'Q35',
+                       u'Devon' : u'Q23156',
+                       u'Dordrecht' : u'Q26421',
+                       u'England' : u'Q21',
+                       u'Flanders (Belgium from 1830)' : u'Q31',
+                       u'Florence' : u'Q2044',
+                       u'France' : u'Q142',
+                       u'Germany' : u'Q183',
+                       u'Great Britain' : u'Q23666',
+                       u'Haarlem' : u'Q9920',
+                       u'Holland' : u'Q102911',
+                       u'Ireland' : u'Q22890',
+                       u'Italy' : u'Q38',
+                       u'Leiden' : u'Q43631',
+                       u'London' : u'Q84',
+                       u'Naples' : u'Q2634',
+                       u'Netherlands' : u'Q55',
+                       u'Paris' : u'Q90',
+                       u'Rome' : u'Q220',
+                       u'Scotland' : u'Q22',
+                       u'Wales' : u'Q25',
+                       u'Windsor' : u'Q464955',
+                       u'Windsor Castle' : u'Q464955'
+                       }
+
+            placeregex = u'\<h4\>Place of origin\<\/h4\>[\s\t\r\n]*\<p\>([^\<]+)\<\/p\>'
+            placematch = re.search(placeregex, itemPageData)
+            if placematch:
+                if placematch.group(1) in places:
+                    metadata[u'madeinqid'] = places.get(placematch.group(1))
+                else:
+                    print (u'Place %s not found' % (placematch.group(1),))
+                    if placematch.group(1) not in missedplaces:
+                        missedplaces[placematch.group(1)]=0
+                    missedplaces[placematch.group(1)] += 1
+
             # Image use policy unclear
             #imageMatch = re.search(imageregex, itemPageData)
             #if imageMatch:
@@ -191,6 +275,8 @@ def getNTGenerator():
             yield metadata
     pywikibot.output(u'Final list of missed locations')
     pywikibot.output(missedlocations)
+    pywikibot.output(u'Final list of missed made in places')
+    pywikibot.output(missedplaces)
 
 def nationalTrustLocationsOnWikidata():
     '''
