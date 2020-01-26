a03b7007048e837b29c1305802b24e6795e3b6e5
multichill <maarten@mdammers.nl>
2020-01-26 22:22:44 +0100

    Patched up the Mauritshuis
    To get the image suggestions working

--- bot/wikidata/mauritshuis_import.py
+++ bot/wikidata/mauritshuis_import.py
@@ -16,13 +16,13 @@ import pywikibot
 import requests
 import re
 import time
-import HTMLParser
+from html.parser import HTMLParser
 
 def getMauritshuisGenerator():
     """
     Generator to return Frans Hals Museum paintings
     """
-    htmlparser = HTMLParser.HTMLParser()
+    htmlparser = HTMLParser()
     searchnlurl = u'https://www.mauritshuis.nl/nl-nl/mapi/zoeken/zoek?categorie=collectie&query=&pagina=70&mh_soort_object[]=7d92939e170e46a595198d756d6919b1&object_nummer='
     searchenurl = u'https://www.mauritshuis.nl/en/mapi/zoeken/zoek?categorie=collectie&query=&hasHistory=true&mh_soort_object=7d92939e170e46a595198d756d6919b1&pagina=70'
 
@@ -34,38 +34,55 @@ def getMauritshuisGenerator():
     pywikibot.output(u'I found %s & %s items to work on, I expected at least 748 items' % (len(nlcollection),
                                                                                            len(encollection),
                                                                                            ))
+    """
     items = {}
-    nlinvregex = u'^http\:\/\/www\.mauritshuis\.nl\/nl-nl\/verdiep\/de-collectie\/kunstwerken\/([^\d]+[^l^\d]|.+-)(l?\d+)/$'
-    eninvregex = u'^http\:\/\/www\.mauritshuis\.nl\/en\/verdiep\/de-collectie\/kunstwerken\/([^\d]+[^l^\d]|.+-)(l?\d+)/$'
-    for nlitem in nlcollection:
-        nlurl = nlitem.get('url')
-        nlinvmatch = re.match(nlinvregex, nlurl)
-        nlinv = nlinvmatch.group(2)
-        if items.get(nlinv):
-            print u'something went wrong'
-            print items.get(nlinv)
-            time.sleep(10)
-        items[nlinv] = {}
-        items[nlinv]['nl']=nlitem
-    for enitem in encollection:
-        enurl = enitem.get('url')
-        eninvmatch = re.match(eninvregex, enurl)
-        eninv = eninvmatch.group(2)
-        items[eninv]['en']=enitem
+    nlinvregex = u'^https\:\/\/www\.mauritshuis\.nl\/nl-nl\/verdiep\/de-collectie\/kunstwerken\/([^\d]+[^l^\d]|.+-)(l?\d+)/?$'
 
+    for nlitem in nlcollection:
+        nlurl = nlitem.get('url').replace('https://www.mauritshuis.nl/nl-NL/Verdiep/De collectie/Kunstwerken/', 'https://www.mauritshuis.nl/nl-nl/verdiep/de-collectie/kunstwerken/').replace(' ', '-')
+        print (nlurl)
+        nlinvmatch = re.match(nlinvregex, nlurl)
+        if nlinvmatch:
+            nlinv = nlinvmatch.group(2)
+            if items.get(nlinv):
+                print (u'something went wrong')
+                print (items.get(nlinv))
+                time.sleep(10)
+            items[nlinv] = {}
+            items[nlinv]['nl']=nlitem
+    """
+    enitems = {}
+    eninvregex = u'^https\:\/\/www\.mauritshuis\.nl\/en\/explore\/the-collection\/artworks\/([^\d]+[^l^\d]|.+-)(l?\d+)/?$'
+    for enitem in encollection:
+        enurl = enitem.get('url').replace('https://www.mauritshuis.nl/en/Verdiep/De collectie/Kunstwerken/', 'https://www.mauritshuis.nl/en/explore/the-collection/artworks/').replace('https://www.mauritshuis.nl/en/verdiep/de-collectie/kunstwerken/', 'https://www.mauritshuis.nl/en/explore/the-collection/artworks/').replace(' ', '-')
+        print (enurl)
+        eninvmatch = re.match(eninvregex, enurl)
+        if eninvmatch:
+            eninv = eninvmatch.group(2)
+            enitems[eninv]=enitem
+        else:
+            print ('%s did not match' % (enurl,))
+    """
     for idnum, dualitem in items.items():
+    """
+    # Just nl for now
+    for nlitem in nlcollection:
+
+        basicnlpage = requests.get(nlitem.get('url'))
+        nlurl = basicnlpage.url
+        print (nlurl)
 
         metadata = {}
 
-        nlitem = dualitem.get(u'nl')
-        enitem = dualitem.get(u'en')
+        #nlitem = dualitem.get(u'nl')
+        #enitem = dualitem.get(u'en')
 
         #if len(item.get(u'authors')) > 2:
         #    print item.get(u'url')
         #    print u'LOOOOOOOONG'
         #    time.sleep(5)
         #print len(item.get(u'authors'))
-        nlurl = nlitem.get(u'url').replace(u'http://www.mauritshuis.nl', u'https://www.mauritshuis.nl')
+        #nlurl = nlitem.get(u'url').replace(u'http://www.mauritshuis.nl', u'https://www.mauritshuis.nl')
         nldetailurl = u'%sdetailgegevens/' % (nlurl,)
 
         # Museum site probably doesn't like it when we go fast
@@ -90,46 +107,65 @@ def getMauritshuisGenerator():
         metadata['id'] = htmlparser.unescape(invnumatch.group(1).strip())
 
         metadata['title'] = { u'nl' : nlitem.get('titel'),
-                              u'en' : enitem.get('titel'),
+                              #u'en' : enitem.get('titel'),
                               }
+        if metadata.get('id') in enitems:
+            metadata['title']['en'] = enitems.get(metadata.get('id')).get('titel')
 
         anoniemregex = u'^([^\(]+)\(([^\)]+)\)$'
-        if len(nlitem.get(u'authors'))==1:
+        if not nlitem.get(u'authors'):
+            metadata['creatorqid'] = u'Q4233718'
+            metadata['description'] = { #u'en' : u'painting by %s' % (enitem.get(u'authors')[0], ),
+                u'nl' : u'schilderij van anoniem'
+            }
+            metadata['creatorname'] = 'anoniem'
+
+        elif len(nlitem.get(u'authors'))==1:
             nlanoniemmatch = re.match(anoniemregex, nlitem.get(u'authors')[0])
-            enanoniemmatch = re.match(anoniemregex, enitem.get(u'authors')[0])
+            #enanoniemmatch = re.match(anoniemregex, enitem.get(u'authors')[0])
 
             if nlitem.get(u'authors')[0].startswith(u'Anoniem'):
                 metadata['creatorqid'] = u'Q4233718'
-                metadata['description'] = { u'en' : u'painting by %s' % (enitem.get(u'authors')[0], ),
+                metadata['description'] = { #u'en' : u'painting by %s' % (enitem.get(u'authors')[0], ),
                                             u'nl' : u'schilderij van %s' % (nlitem.get(u'authors')[0], ),
                                             }
+                metadata['creatorname'] = nlitem.get(u'authors')[0]
 
-            elif nlanoniemmatch and enanoniemmatch:
-                metadata['description'] = { u'en' : u'painting %s %s' % (enanoniemmatch.group(2).strip(),
-                                                                         enanoniemmatch.group(1).strip(),),
+            elif nlanoniemmatch:# and enanoniemmatch:
+                metadata['description'] = { #u'en' : u'painting %s %s' % (enanoniemmatch.group(2).strip(),
+                                            #                             enanoniemmatch.group(1).strip(),),
                                             u'nl' : u'schilderij %s %s' % (nlanoniemmatch.group(2).strip(),
                                                                         nlanoniemmatch.group(1).strip(),),
                                             }
+                metadata['creatorname'] = '%s %s' % (nlanoniemmatch.group(2).strip(),
+                                                     nlanoniemmatch.group(1).strip(),)
             else:
-                metadata['description'] = { u'en' : u'painting by %s' % (enitem.get(u'authors')[0], ),
+                metadata['description'] = { u'en' : u'painting by %s' % (nlitem.get(u'authors')[0], ),
                                             u'nl' : u'schilderij van %s' % (nlitem.get(u'authors')[0], ),
                                             }
+                metadata['creatorname'] = nlitem.get(u'authors')[0]
         elif len(nlitem.get(u'authors'))==2:
-            metadata['description'] = { u'en' : u'painting by %s & %s' % (enitem.get(u'authors')[0],
-                                                                          enitem.get(u'authors')[1]),
+            metadata['description'] = { #u'en' : u'painting by %s & %s' % (enitem.get(u'authors')[0],
+                                        #                                  enitem.get(u'authors')[1]),
                                         u'nl' : u'schilderij van %s & %s' % (nlitem.get(u'authors')[0],
                                                                              nlitem.get(u'authors')[1],),
                                         }
+            metadata['creatorname'] =  '%s & %s' % (nlitem.get(u'authors')[0],
+                                                    nlitem.get(u'authors')[1],)
         else:
-            metadata['description'] = { u'en' : u'painting by %s, %s & %s' % (enitem.get(u'authors')[0],
-                                                                              enitem.get(u'authors')[1],
-                                                                              enitem.get(u'authors')[2]),
+            metadata['description'] = { #u'en' : u'painting by %s, %s & %s' % (enitem.get(u'authors')[0],
+                                        #                                      enitem.get(u'authors')[1],
+                                        #                                      enitem.get(u'authors')[2]),
                                         u'nl' : u'schilderij van %s, %s & %s' % (nlitem.get(u'authors')[0],
                                                                                  nlitem.get(u'authors')[1],
                                                                                  nlitem.get(u'authors')[2]),
                                         }
+            metadata['creatorname'] = '%s, %s & %s' % (nlitem.get(u'authors')[0],
+                                                       nlitem.get(u'authors')[1],
+                                                       nlitem.get(u'authors')[2],)
 
-        metadata['inception'] = nlitem.get(u'periode')
+        # Fixme, better date handling
+        # metadata['inception'] = nlitem.get(u'periode')
 
         dimensionregex = u'\<div class\=\"component-key-value-set\"\>[\s\t\r\n]*\<div class\=\"key\"\>Afmetingen\<\/div\>[\s\t\r\n]*\<div class\=\"value\">hoogte\:\s*(?P<height>\d+(,\d+)?)\s*cm[\s\t\r\n]*\<br \/\>breedte\:\s*(?P<width>\d+(,\d+)?)\s*cm[\s\t\r\n]*\<\/div\>'
         dimensionmatch = re.search(dimensionregex, itempage.text)
@@ -153,6 +189,12 @@ def getMauritshuisGenerator():
         # Only return if a valid medium is found
         if oiloncanvasmatch:
             metadata['medium'] = u'oil on canvas'
+        if nlitem.get(u'thumbnailUrl'):
+            metadata[u'imageurl'] = nlitem.get(u'thumbnailUrl').replace('/-/media/', 'https://www.mauritshuis.nl/-/media/').replace('mw=300&mh=300', 'dl=1')
+            metadata[u'imageurlformat'] = u'Q2195' #JPEG
+            metadata[u'imageoperatedby'] = u'Q221092'
+            #metadata[u'imageurlforce'] = True
+
         yield metadata
 
 
@@ -160,7 +202,7 @@ def main():
     dictGen = getMauritshuisGenerator()
 
     #for painting in dictGen:
-    #    print painting
+    #    print (painting)
 
     artDataBot = artdatabot.ArtDataBot(dictGen, create=True)
     artDataBot.run()
