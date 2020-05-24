813cee0885e2eb9ef5a39b25fc29df1c9ab5ecad
multichill <maarten@mdammers.nl>
2020-05-24 21:56:41 +0200

    Correct location based on collection

--- bot/wikidata/location_from_collection.py
+++ bot/wikidata/location_from_collection.py
@@ -15,13 +15,15 @@ class LocationFromCollectionBot:
     """
     A bot to add locations to paintings on Wikidata
     """
-    def __init__(self, generator):
+    def __init__(self, generator, correctlocation=False):
         """
         Arguments:
             * generator    - A generator that yields ItemPage objects.
+            * correctlocation - Correct the location with the contents of the collection?
 
         """
         self.generator = generator
+        self.correctlocation = correctlocation
         self.repo = pywikibot.Site().data_repository()
                 
     def run(self):
@@ -34,42 +36,64 @@ class LocationFromCollectionBot:
             claims = data.get('claims')
 
             # Do some checks
-            if u'P276' in claims:
-                pywikibot.output(u'Already has a location, done')
-                continue
             if u'P195' not in claims:
                 pywikibot.output(u'No collection found, skipping')
                 continue
             if len(claims[u'P195'])!=1:
                 pywikibot.output(u'Found multiple collections, skipping')
                 continue
+            if u'P276' in claims:
+                if not self.correctlocation:
+                    pywikibot.output(u'Already has a location and not correcting, done')
+                    continue
+                elif len(claims[u'P276'])!=1:
+                    pywikibot.output(u'Found multiple locations to correct, skipping')
+                    continue
+                else:
+                    locationclaim = claims.get('P276')[0]
+                    currentlocation = locationclaim.getTarget()
+                    collection = claims.get('P195')[0].getTarget()
+                    # TODO: Figure out how to get the magic edit summary on Wikidata
+                    summary = 'replacing [[Wikidata:WikiProject sum of all paintings/Imprecise location|imprecise location]] [[%s]]' % (currentlocation.title(),)
+                    pywikibot.output(u'Adding collection %s as new location' % (collection.title(),))
+                    locationclaim.changeTarget(collection, summary=summary)
+            else:
+                #Get the collection so we can add it later
+                collection = claims.get('P195')[0].getTarget()
+                summary = u'based on collection (which has coordinates)'
+                newclaim = pywikibot.Claim(self.repo, u'P276')
+                newclaim.setTarget(collection)
+                pywikibot.output(u'Adding collection %s as location' % (collection.title(),))
+                itempage.addClaim(newclaim, summary=summary)
 
-            #Get the collection so we can add it later
-            collection = claims.get('P195')[0].getTarget()
-            summary = u'based on collection (which has coordinates)'
-            newclaim = pywikibot.Claim(self.repo, u'P276')
-            newclaim.setTarget(collection)
-            pywikibot.output(u'Adding collection %s as location' % (collection.title(),))
-            itempage.addClaim(newclaim, summary=summary)
 
-
-def main():
+def main(*args):
     """
     Main function. Grab a generator and pass it to the bot to work on
     """
-    
+    correctlocation = False
     query = u"""SELECT ?item ?collection  WHERE {
   ?item wdt:P31 wd:Q3305213 .
   ?item wdt:P195 ?collection .
   MINUS { ?item wdt:P276 [] } .
-  MINUS { ?item wdt:P195 wd:Q842858 } . # Skipping Nationalmuseum for now
-  MINUS { ?item wdt:P195 wd:Q18600731 } . # And RCE art collection, don't like that as location
   ?collection wdt:P625 [] .
 } ORDER BY ?collection"""
+
+    for arg in pywikibot.handle_args(args):
+        if arg.startswith('-correctlocation'):
+            correctlocation = True
+            query = """SELECT ?item ?collection ?location WHERE {
+  ?item wdt:P31 wd:Q3305213 .
+  ?item wdt:P276 ?location .
+  ?item wdt:P195 ?collection . ?collection wdt:P131+ ?location ;
+                                           wdt:P625 [] .
+  } ORDER BY ?collection
+LIMIT 5000"""
+
     repo = pywikibot.Site().data_repository()
     generator = pagegenerators.PreloadingItemGenerator(pagegenerators.WikidataSPARQLPageGenerator(query, site=repo))
 
-    locationFromCollectionBot = LocationFromCollectionBot(generator)
+    locationFromCollectionBot = LocationFromCollectionBot(generator, correctlocation=correctlocation)
     locationFromCollectionBot.run()
 
 if __name__ == "__main__":
