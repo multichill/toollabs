5f90ad54765364dbc3a4c3d5b52c718e1d1abd47
multichill <maarten@mdammers.nl>
2021-05-16 15:40:02 +0200

    Update the Rijksmuseum import bot to add some more data

--- bot/wikidata/rijksmuseum_import.py
+++ bot/wikidata/rijksmuseum_import.py
@@ -93,11 +93,16 @@ def getRijksmuseumGenerator():
                 metadata['creatorname'] = record.get('principalOrFirstMaker')
 
                 metadata['description'] = { u'nl' : u'%s van %s' % (u'schilderij', metadata.get('creatorname'),),
-                                           u'en' : u'%s by %s' % (u'painting', metadata.get('creatorname'),),
+                                            u'en' : u'%s by %s' % (u'painting', metadata.get('creatorname'),),
+                                            u'de' : u'%s von %s' % (u'Gemälde', metadata.get('creatorname'),),
+                                            u'fr' : u'%s de %s' % (u'peinture', metadata.get('creatorname'),),
                                             }
             # FIXME : This will only catch oil on canvas
-            if record.get('physicalMedium')==u'olieverf op doek':
-                metadata['medium'] = u'oil on canvas'
+            if record.get('physicalMedium'):
+                if record.get('physicalMedium')==u'olieverf op doek':
+                    metadata['medium'] = u'oil on canvas'
+                elif record.get('physicalMedium')==u'olieverf op paneel':
+                    metadata['medium'] = u'oil on panel'
 
             # Set the inception only if start only start or if start and end are the same
             if record.get('dating') and \
@@ -130,17 +135,52 @@ def getRijksmuseumGenerator():
                     if record.get('acquisition').get('creditLine')==u'Bruikleen van de gemeente Amsterdam (legaat A. van der Hoop)':
                         metadata['extracollectionqid'] = u'Q19750488'
 
+            # Made in. Only add it if one location is given
+            if record.get('productionPlaces') and len(record.get('productionPlaces'))==1:
+                productionPlace = record.get('productionPlaces')[0]
+
+                # Based on the facets in the search output
+                madein = { 'Amsterdam' : 'Q727',
+                           'Antwerpen (stad)' : 'Q12892',
+                           'Batavia' : 'Q1199713',
+                           'Brugge' : 'Q12994',
+                           'Brussel (stad)' : 'Q239',
+                           'Den Haag' : 'Q36600',
+                           'Dordrecht' : 'Q26421',
+                           'Duitsland' : 'Q183',
+                           'Florence' : 'Q2044',
+                           'Frankrijk' : 'Q142',
+                           'Haarlem' : 'Q9920',
+                           'Istanboel' : 'Q406',
+                           'Italië' : 'Q38',
+                           'Jogjakarta' : 'Q7568',
+                           'Lage Landen' : 'Q476033',
+                           'Leiden' : 'Q43631',
+                           'Londen' : 'Q84',
+                           'Holland' : 'Q102911',
+                           'Nederland' : 'Q55',
+                           'Noordelijke Nederlanden' : 'Q27996474',
+                           'Parijs' : 'Q90',
+                           'Utrecht' : 'Q803',
+                           'Rome' : 'Q220',
+                           'Siena' : 'Q2751',
+                           'Venetië' : 'Q641',
+                           'Zuidelijke Nederlanden' : 'Q6581823',
+                           }
+                if productionPlace in madein:
+                    metadata['madeinqid'] = madein.get(productionPlace)
+
             # Get the dimensions
             if record.get('dimensions'):
                 for dimension in record.get('dimensions'):
                     if dimension.get('unit') == u'cm' and \
                        dimension.get('type') == u'hoogte' and \
-                       dimension.get('part') == u'drager' and \
+                       (dimension.get('part') == u'drager' or dimension.get('part') == u'origineel') and \
                        dimension.get('value') :
                         metadata['heightcm'] = dimension.get('value').replace(u',', u'.')
                     elif dimension.get('unit') == u'cm' and \
                          dimension.get('type') == u'breedte' and \
-                         dimension.get('part') == u'drager' and \
+                         (dimension.get('part') == u'drager' or dimension.get('part') == u'origineel') and \
                          dimension.get('value') :
                         metadata['widthcm'] = dimension.get('value').replace(u',', u'.')
                 if not metadata.get('heightcm') and not metadata.get('widthcm') and len(record.get('dimensions'))==2:
@@ -158,14 +198,23 @@ def getRijksmuseumGenerator():
             yield metadata
     return
     
-def main():
+def main(*args):
     dictGen = getRijksmuseumGenerator()
+    dryrun = False
+    create = False
 
-    #for painting in dictGen:
-    #    print (painting)
+    for arg in pywikibot.handle_args(args):
+        if arg.startswith('-dry'):
+            dryrun = True
+        elif arg.startswith('-create'):
+            create = True
 
-    artDataBot = artdatabot.ArtDataBot(dictGen, create=True)
-    artDataBot.run()
+    if dryrun:
+        for painting in dictGen:
+            print (painting)
+    else:
+        artDataBot = artdatabot.ArtDataBot(dictGen, create=create)
+        artDataBot.run()
 
 if __name__ == "__main__":
     main()
