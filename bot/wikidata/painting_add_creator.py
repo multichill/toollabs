fc6e03e0bbf7e20aeca5571ce32fe328b650121f
multichill <maarten@mdammers.nl>
2022-04-16 16:48:02 +0200

    Fix page generator

--- bot/wikidata/painting_add_creator.py
+++ bot/wikidata/painting_add_creator.py
@@ -248,7 +248,7 @@ class PaintingBot:
 
         # Search Wikidata for a suitable candidate, tell the search to only return humans
         searchstring = u'%s haswbstatement:P31=Q5' % (creator,)
-        creategen = pagegenerators.PreloadingEntityGenerator(pagegenerators.WikibaseItemGenerator(pagegenerators.SearchPageGenerator(searchstring, step=None, total=50, namespaces=[0], site=self.repo)))
+        creategen = pagegenerators.PreloadingEntityGenerator(pagegenerators.WikibaseItemGenerator(pagegenerators.SearchPageGenerator(searchstring, total=50, namespaces=[0], site=self.repo)))
 
         for creatoritem in creategen:
             if creatoritem.isRedirectPage():
