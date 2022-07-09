359bf7b4e037cfcf59d5b71160d2cf08524df927
multichill <maarten@mdammers.nl>
2022-07-09 18:23:00 +0200

    Break up the RKDimages bot into two parts. Still work in progress

--- bot/wikidata/rkdimages_importer.py
+++ bot/wikidata/rkdimages_importer.py
@@ -21,25 +21,69 @@ import time
 import artdatabot
 
 
-class RKDImagesImporterBot(artdatabot.ArtDataBot):
+class ArtDataExpanderBot(artdatabot.ArtDataBot):
     """
-    A bot to enrich humans on Wikidata
+    Wrapper around ArtDataBot that only expands items. Will probably refactor at some point
+    """
+    def __init__(self, generator):
+        """
+        Arguments:
+            * generator    - A generator that yields Dict objects.
+            The dict in this generator needs to contain 'itempage' with an ItemPage object
+        """
+        self.generator = generator
+        self.repo = pywikibot.Site().data_repository()
+
+    def run(self):
+        """
+        Starts the robot.
+        """
+        for metadata in self.generator:
+            itempage = metadata.pop('itempage', None)
+            if itempage:
+                metadata = super().enrichMetadata(metadata)
+                super().updateArtworkItem(itempage, metadata)
+
+
+class RKDImagesExpanderGenerator():
+    """
+    A generator for data from RKDimages
     """
     def __init__(self, generator):
         """
         Arguments:
             * generator    - A generator that yields ItemPage objects.
-
         """
         self.generator = generator
         self.rkd_artists = self.get_rkd_artists()
-        self.artwork_metadata_generator = self.get_artwork_metadata_generator(generator)
         self.repo = pywikibot.Site().data_repository()
+        self.object_types = {'painting': {'qid': 'Q3305213',
+                                          'labels': {'en': 'painting',
+                                                     'nl': 'schilderij',
+                                                     'de': 'Gemälde',
+                                                     'fr': 'peinture',
+                                                     }
+                                          },
+                             'drawing': {'qid': 'Q93184',
+                                         'labels': {'en': 'drawing', 'nl': 'tekening'}
+                                         },
+                             }
+        self.connector_word = {'nl': 'van', 'en': 'by', 'de': 'von', 'fr': 'de'}
+        self.creator_qualifiers = {'test1234': {}, } # TODO: Implement
+        self.mediums = {'canvas, oil paint': 'oil on canvas',
+                        'panel, oil paint': 'oil on panel',
+                        'panel (oak), oil paint': 'oil on oak panel',
+                        'panel (lime), oil paint': 'oil on lime panel',
+                        'copper, oil paint': 'oil on copper',
+                        'panel (poplar), oil paint': 'oil on poplar panel',
+                        'paper, aquarel paint (watercolor)': 'watercolor on paper',
+                        'cardboard, oil paint': 'oil on cardboard',
+                        }
 
     def get_rkd_artists(self):
         """
         Make a lookup table for RKDartists
-        :return:
+        :return: The lookup table as a dict
         """
         result = {}
         query = """SELECT ?item ?id WHERE { ?item wdt:P650 ?id }"""
@@ -51,17 +95,18 @@ class RKDImagesImporterBot(artdatabot.ArtDataBot):
             result[result_item.get('id')] = qid
         return result
 
-    def get_artwork_metadata_generator(self, generator):
+    def __iter__(self):
         """
         Loop over the items in the generator, get more metadata and return the result
         :param generator: itempages with and RKDimages link
         :yield: Tuple to be processed
         """
-        for itempage in generator:
+
+        for itempage in self.generator:
             pywikibot.output('Working on %s' % (itempage.title(),))
 
             if not itempage.exists():
-                pywikibot.output(u'Item does not exist, skipping')
+                pywikibot.output('Item does not exist, skipping')
                 continue
 
             data = itempage.get()
@@ -69,25 +114,31 @@ class RKDImagesImporterBot(artdatabot.ArtDataBot):
 
             # Do some checks so we are sure we found exactly one inventory number and one collection
             if 'P350' not in claims:
-                pywikibot.output(u'No RKDimages found, skipping')
+                pywikibot.output('No RKDimages found, skipping')
                 continue
 
             rkdimagesid = claims.get(u'P350')[0].getTarget()
             metadata = self.get_rkdimage_metadata(rkdimagesid)
             if metadata:
-                yield itempage, metadata
+                metadata['itempage'] = itempage
+                yield metadata
 
     def get_rkdimage_metadata(self, rkdimagesid):
         """
         :param rkdimagesid:
         :return:
         """
-        rkdimages_url = 'https://api.rkd.nl/api/record/images/%s?format=json' % (rkdimagesid,)
+        # Force to English for consistent output
+        rkdimages_url = 'https://api.rkd.nl/api/record/images/%s?format=json&language=en' % (rkdimagesid,)
         refurl = 'https://rkd.nl/explore/images/%s' % (rkdimagesid,)
 
         # Do some checking if it actually exists?
         rkdimages_page = requests.get(rkdimages_url)
-        rkdimages_json = rkdimages_page.json()
+        try:
+            rkdimages_json = rkdimages_page.json()
+        except ValueError: # Throws simplejson.errors.JSONDecodeError
+            pywikibot.output('Got invalid json for%s, skipping' % (rkdimagesid,))
+            return None
 
         if rkdimages_json.get('content') and rkdimages_json.get('content').get('message'):
             pywikibot.output(u'Something went wrong, got "%s" for %s, skipping' % (rkdimages_json.get('content').get('message'),
@@ -98,56 +149,146 @@ class RKDImagesImporterBot(artdatabot.ArtDataBot):
 
         metadata = {}
         metadata['refurl'] = refurl
-        metadata['title'] = {}
+
+        metadata.update(self.get_instance_of(rkdimages_docs))
+        metadata.update(self.get_title(rkdimages_docs))
+        metadata.update(self.get_creator(rkdimages_docs))
+        metadata.update(self.get_description(metadata))
+        metadata.update(self.get_medium(rkdimages_docs))
+        metadata.update(self.get_genre(rkdimages_docs))
+
+        return metadata
+
+    def get_instance_of(self, rkdimages_docs):
+        """
+        :param rkdimages_docs:
+        :return:
+        """
+        metadata = {}
+        if rkdimages_docs.get('objectcategorie') and len(rkdimages_docs.get('objectcategorie'))==1:
+            object_type = rkdimages_docs.get('objectcategorie')[0]
+            if object_type in self.object_types:
+                metadata['instanceofqid'] = self.object_types.get(object_type).get('qid')
+                metadata['instanceofnames'] = self.object_types.get(object_type).get('labels')
+        return metadata
+
+    def get_title(self, rkdimages_docs):
+        """
+        Get the title in Dutch and English
+        :param rkdimages_docs: The metadata from RKDimages
+        :return: Dict
+        """
+        metadata = {}
+        nl_title = None
+        en_title = None
 
         if rkdimages_docs.get('benaming_kunstwerk') and rkdimages_docs.get('benaming_kunstwerk')[0]:
-            metadata['title']['nl'] = rkdimages_docs.get('benaming_kunstwerk')[0]
+            nl_title = rkdimages_docs.get('benaming_kunstwerk')[0]
         if rkdimages_docs.get('titel_engels'):
-            metadata['title']['en'] = rkdimages_docs.get('titel_engels')
+            en_title = rkdimages_docs.get('titel_engels')
 
-        if rkdimages_docs.get('toeschrijving') and len(rkdimages_docs.get('toeschrijving')) == 1 and rkdimages_docs.get('toeschrijving')[0]:
+        if nl_title or en_title:
+            metadata['title'] = {}
+            if nl_title:
+                metadata['title']['nl'] = nl_title
+            if en_title:
+                metadata['title']['en'] = en_title
+
+        return metadata
+
+    def get_creator(self, rkdimages_docs):
+        """
+        Get the (sometimes uncertain) creator of the work
+        :param rkdimages_docs: The metadata from RKDimages
+        :return: Dict
+        """
+        metadata = {}
+        if rkdimages_docs.get('toeschrijving') and len(rkdimages_docs.get('toeschrijving')) == 1:
             toeschrijving = rkdimages_docs.get('toeschrijving')[0]
             print (toeschrijving)
             toeschrijving.pop('opmerking_toeschrijving', None)
-            if toeschrijving.get('status') and toeschrijving.get('status')=='huidig':
-                if toeschrijving.get('naam') and toeschrijving.get('naam')=='Anoniem':
+            if toeschrijving.get('status') and toeschrijving.get('status') == 'huidig':
+                if toeschrijving.get('naam') and toeschrijving.get('naam') == 'Anoniem':
                     metadata['creatorqid'] = 'Q4233718'
-                elif len(toeschrijving.keys())==4 and toeschrijving.get('naam') and \
+                elif len(toeschrijving.keys()) == 4 and toeschrijving.get('naam') and \
                         toeschrijving.get('naam_linkref') and toeschrijving.get('naam_inverted'):
                     if toeschrijving.get('naam_linkref') in self.rkd_artists:
                         metadata['creatorqid'] = self.rkd_artists.get(toeschrijving.get('naam_linkref'))
                     metadata['creatorname'] = toeschrijving.get('naam_inverted')
-                    metadata['description'] = { 'nl' : '%s van %s' % ('schilderij', metadata.get('creatorname'),),
-                                                'en' : '%s by %s' % ('painting', metadata.get('creatorname'),),
-                                                'de' : '%s von %s' % ('Gemälde', metadata.get('creatorname'),),
-                                                'fr' : '%s de %s' % ('peinture', metadata.get('creatorname'),),
-                                                }
+
                 elif len(toeschrijving.keys())==7 and toeschrijving.get('naam') and toeschrijving.get('naam_linkref') \
                         and toeschrijving.get('naam_inverted') and toeschrijving.get('kwalificatie_en') and \
                         toeschrijving.get('kwalificatie') and toeschrijving.get('kwalificatie_linkref'):
-                        metadata['creatorname'] = toeschrijving.get('naam_inverted')
+                        metadata['uncertaincreatorname'] = toeschrijving.get('naam_inverted')
+                        creator_qualifier = toeschrijving.get('kwalificatie_en')
+                        if creator_qualifier in self.creator_qualifiers:
+                            metadata['creatorqualifierpid'] = self.creator_qualifiers.get(creator_qualifier).get('pid')
+                            metadata['creatorqualifiernames'] = self.creator_qualifiers.get(creator_qualifier).get('labels')
+                        else:
+                            metadata['creatorqualifiernames'] = {'en': creator_qualifier, }
+                        # Always set the Dutch description to how it set at the source
+                        metadata['creatorqualifiernames']['nl'] = toeschrijving.get('kwalificatie')
                         if toeschrijving.get('naam_linkref') in self.rkd_artists:
                             metadata['uncertaincreatorqid'] = self.rkd_artists.get(toeschrijving.get('naam_linkref'))
-                        if toeschrijving.get('kwalificatie_en') == 'after' and toeschrijving.get('kwalificatie') == 'naar':
-                            metadata['description'] = { 'nl' : 'schilderij naar %s' % (metadata.get('creatorname'),),
-                                                        'en' : 'painting after %s' % (metadata.get('creatorname'),),
-                                                    }
+                        #if toeschrijving.get('kwalificatie_en') == 'after' and toeschrijving.get('kwalificatie') == 'naar':
+                        #    metadata['description'] = { 'nl' : 'schilderij naar %s' % (metadata.get('creatorname'),),
+                        #                                'en' : 'painting after %s' % (metadata.get('creatorname'),),
+                        #                            }
 
-
-
-
-
-        print (metadata)
 
         return metadata
 
-    def run(self):
+    def get_description(self, input_metadata):
         """
-        Starts the robot.
+
+        :param rkdimages_docs:
+        :return:
         """
-        for itempage, metadata in self.artwork_metadata_generator:
-            metadata = super().enrichMetadata(metadata)
-            super().updateArtworkItem(itempage, metadata)
+        metadata = {}
+        if not input_metadata.get('instanceofnames'):
+            return metadata
+        if input_metadata.get('creatorname'):
+            metadata['description'] = {}
+            creatorname = input_metadata.get('creatorname')
+            for lang in input_metadata.get('instanceofnames'):
+                if lang in self.connector_word:
+                    object_type = input_metadata.get('instanceofnames').get(lang)
+                    connector_word = self.connector_word.get(lang)
+                    metadata['description'][lang] = '%s %s %s' % (object_type, connector_word, creatorname)
+        elif input_metadata.get('uncertaincreatorname') and input_metadata.get('creatorqualifiernames'):
+            metadata['description'] = {}
+            creatorname = input_metadata.get('uncertaincreatorname')
+            for lang in input_metadata.get('instanceofnames'):
+                if lang in input_metadata.get('creatorqualifiernames'):
+                    object_type = input_metadata.get('instanceofnames').get(lang)
+                    connector_word = input_metadata.get('creatorqualifiernames').get(lang)
+                    metadata['description'][lang] = '%s %s %s' % (object_type, connector_word, creatorname)
+        return metadata
+
+    def get_medium(self, rkdimages_docs):
+        """
+
+        :param rkdimages_docs:
+        :return:
+        """
+        metadata = {}
+        if rkdimages_docs.get('virtualFields') and rkdimages_docs.get('virtualFields').get('drager_techniek'):
+            medium = rkdimages_docs.get('virtualFields').get('drager_techniek').get('contents')
+            if medium:  # Sometimes it's just empty
+                if medium in self.mediums:
+                    medium = self.mediums.get(medium)
+                metadata['medium'] = medium
+        return metadata
+
+    def get_genre(self, rkdimages_docs):
+        """
+
+        :param rkdimages_docs:
+        :return:
+        """
+        metadata = {}
+        return metadata
+
 
 
     def isRemovableSource(self, source):
@@ -167,10 +308,14 @@ def main(*args):
     """
     Main function. Grab a generator and pass it to the bot to work on
     """
+
+    dryrun = False
     create = False
     source = False
     for arg in pywikibot.handle_args(args):
-        if arg=='-create':
+        if arg.startswith('-dry'):
+            dryrun = True
+        elif arg=='-create':
             create = True
         elif arg=='-source':
             source = True
@@ -250,17 +395,34 @@ def main(*args):
   }
 }
 LIMIT 100000"""
-        query2 = """SELECT DISTINCT ?item WHERE {
+        query = """SELECT DISTINCT ?item WHERE {
   ?item wdt:P350 ?value.
   MINUS {
     ?item wdt:P170 []
   }
 }
 LIMIT 10000"""
+        query2 = """SELECT ?item WHERE {
+  ?item wdt:P350 ?id ;
+        p:P170 ?creatorstatement .
+    FILTER NOT EXISTS { ?creatorstatement prov:wasDerivedFrom ?derivedFrom .}
+} LIMIT 10"""
+        query = """SELECT ?item WHERE {
+  ?item wdt:P350 ?id ;
+        p:P31 ?statement .
+    FILTER NOT EXISTS { ?statement prov:wasDerivedFrom ?derivedFrom .}
+} LIMIT 10000"""
         generator = pagegenerators.PreloadingEntityGenerator(pagegenerators.WikidataSPARQLPageGenerator(query, site=repo))
 
-    rkd_images_importer_bot = RKDImagesImporterBot(generator)
-    rkd_images_importer_bot.run()
+    metadata_generator = RKDImagesExpanderGenerator(generator)
+    #metadata_generator.run()
+
+    if dryrun:
+        for metadata in metadata_generator:
+            print(metadata)
+    else:
+        art_data_expander_bot = ArtDataExpanderBot(metadata_generator)
+        art_data_expander_bot.run()
 
 if __name__ == "__main__":
     main()
