1879f7e35d8149d6a92c94b73e6ba6bf9fa60b41
multichill <maarten@mdammers.nl>
2022-07-27 12:31:43 +0200

    Expand inception sort to also fix somevalue cases

--- bot/wikidata/inception_sort.py
+++ bot/wikidata/inception_sort.py
@@ -18,9 +18,9 @@ class InceptionSortBot(WikidataBot):
         """
         No arguments, bot makes it's own generator based on the genres
         """
-        super(InceptionSortBot, self).__init__()
-        self.use_from_page = False
-        self.generator = self.getGenerator()
+        self.repo = pywikibot.Site().data_repository()
+        self.sort_generator = self.get_sort_generator()
+        self.somevalue_generator = self.get_somevalue_generator()
         self.precisionYears = { 6 : 1000,
                                 7 : 100,
                                 8 : 10,
@@ -29,43 +29,61 @@ class InceptionSortBot(WikidataBot):
                                 11 : 1,
                                 }
 
-    def getGenerator(self):
+    def get_sort_generator(self):
         """
         Get a generator of inceptions to be fixed.
         :return: A generator that yields ItemPages
         """
-        query = u"""
-SELECT ?item ?inception ?earliestinception ?latestinception WHERE {
-  ?item wdt:P31 wd:Q3305213 .
-  ?item p:P571 ?inceptionstatement .
-  ?inceptionstatement ps:P571 ?inception .
-  ?inceptionstatement pq:P1319 ?earliestinception .
-  ?inceptionstatement pq:P1326 ?latestinception .
-  FILTER(!(?earliestinception <= ?inception && ?inception  <= ?latestinception ))
-  FILTER(?earliestinception < ?latestinception) .
-  FILTER(YEAR(?inception) > 1200)
-  } ORDER BY ?inception
-  LIMIT 5000"""
-
+        query = u"""SELECT ?item ?inception ?earliestinception ?latestinception WHERE {
+  ?item wdt:P31 wd:Q3305213 ;
+        p:P571 [ ps:P571 ?inception ; pq:P1319 ?earliestinception ; pq:P1326 ?latestinception ].
+  FILTER(YEAR(?inception) > 1200 && (!(?earliestinception <= ?inception && ?inception  <= ?latestinception )) && ?earliestinception < ?latestinception) .  
+  } 
+  LIMIT 1000"""
         generator = pagegenerators.PreloadingEntityGenerator(pagegenerators.WikidataSPARQLPageGenerator(query,
                                                                                                       site=self.repo))
         return generator
 
-    def treat_page_and_item(self, page, item):
-        """Treat each item, page is probably None."""
+    def get_somevalue_generator(self):
+        """
+        Get a generator of inceptions to be fixed.
+        :return: A generator that yields ItemPages
+        """
+        query = """SELECT ?item ?inception ?earliestinception ?latestinception WHERE {
+  ?item wdt:P31 wd:Q3305213 ;
+        p:P571 [ ps:P571 ?inception ; pq:P1319 ?earliestinception ; pq:P1326 ?latestinception ].
+  FILTER(wikibase:isSomeValue(?inception) && ?earliestinception < ?latestinception && YEAR(?earliestinception) > 1200 && YEAR(?latestinception) > 1200)
+  } LIMIT 1000"""
+        generator = pagegenerators.PreloadingEntityGenerator(pagegenerators.WikidataSPARQLPageGenerator(query,
+                                                                                                        site=self.repo))
+        return generator
+
+    def run(self):
+        """
+        Starts the robot.
+        """
+        for item in self.sort_generator:
+            self.fix_inception_sort(item)
+
+        for item in self.somevalue_generator:
+            self.fix_inception_somevalue(item)
+
+
+    def fix_inception_sort(self, item):
+        """Treat each item."""
         if not item.exists():
             return
 
         data = item.get()
         claims = data.get('claims')
 
-        if not u'P571' in claims:
+        if 'P571' not in claims:
             return
 
-        if len(claims.get(u'P571')) > 1:
+        if len(claims.get('P571')) > 1:
             return
 
-        inceptionclaim = claims.get(u'P571')[0]
+        inceptionclaim = claims.get('P571')[0]
         earliestqualifier = inceptionclaim.qualifiers.get('P1319')[0]
         latestqualifier = inceptionclaim.qualifiers.get('P1326')[0]
 
@@ -73,15 +91,12 @@ SELECT ?item ?inception ?earliestinception ?latestinception WHERE {
         earliestdate = earliestqualifier.getTarget()
         latestdate = latestqualifier.getTarget()
 
-        claimjson = inceptionclaim.toJSON()
-        #pywikibot.output(claimjson)
-
         if inceptiondate.precision < 6:
-            pywikibot.output(u'Precision shouldn\'t be smaller than millenia, skipping')
+            pywikibot.output(u'Precision should at least be millenia, skipping')
             return
 
         if not inceptiondate.precision < 9:
-            pywikibot.output(u'Precision should be smaller than year, skipping')
+            pywikibot.output('Precision should be less precise than year, skipping')
             return
 
         if not earliestdate.precision > inceptiondate.precision or not latestdate.precision > inceptiondate.precision or \
@@ -94,8 +109,8 @@ SELECT ?item ?inception ?earliestinception ?latestinception WHERE {
 
         inceptionPrecisionYears = self.precisionYears.get(inceptiondate.precision)
         inceptionCurrentYear = inceptiondate.year
-        inceptionStartYear = abs(inceptiondate.year/inceptionPrecisionYears) * inceptionPrecisionYears
-        inceptionEndYear = inceptionStartYear + inceptionPrecisionYears
+        inceptionStartYear = inceptiondate.year - inceptionPrecisionYears
+        inceptionEndYear = inceptiondate.year + inceptionPrecisionYears
 
         earliestPrecisionYears = self.precisionYears.get(earliestdate.precision)
         if earliestPrecisionYears==1:
@@ -132,6 +147,62 @@ SELECT ?item ?inception ?earliestinception ?latestinception WHERE {
         inceptiondate.year = averageYear
         pywikibot.output(summary)
         inceptionclaim.changeTarget(inceptiondate, summary=summary)
+
+    def fix_inception_somevalue(self, item):
+        """Treat each item."""
+        if not item.exists():
+            return
+
+        data = item.get()
+        claims = data.get('claims')
+
+        if 'P571' not in claims:
+            return
+
+        if len(claims.get('P571')) > 1:
+            return
+
+        inceptionclaim = claims.get('P571')[0]
+        if inceptionclaim.getTarget() or inceptionclaim.getSnakType()!='somevalue':
+            pywikibot.output('I expected somevalue, but I got something else')
+            return
+        earliestqualifier = inceptionclaim.qualifiers.get('P1319')[0]
+        latestqualifier = inceptionclaim.qualifiers.get('P1326')[0]
+
+        # Just use the year and don't look at precision here
+        earliestyear = earliestqualifier.getTarget().year
+        latestyear = latestqualifier.getTarget().year
+        averageYear = int(abs((earliestyear + latestyear)/2))
+
+        # For things like 1901-2000 I want to end with century
+        incstart = str(earliestyear)
+        normalend = str(latestyear)
+        lowerend = str(latestyear-1)
+
+        if len(incstart) != 4 or len(normalend) != 4:
+            return
+        precision = 5 # 10 millenia
+
+        # The normal loop
+        if incstart[0] == normalend[0]:
+            precision = 6  # millenium
+            if incstart[1] == normalend[1]:
+                precision = 7  # century
+                if incstart[2] == normalend[2]:
+                    precision = 8  # decade
+        # The one lower loop. Can't mix them, will give funky results with things like 1701 and 1800
+        if incstart[0] == lowerend[0]:
+            if precision < 6:
+                precision = 6  # millenium
+            if incstart[1] == lowerend[1]:
+                if precision < 7:
+                    precision = 7  # century
+                # Don't do it for decade
+
+        newdate = pywikibot.WbTime(year=averageYear, precision=precision)
+        summary = 'Replacing somevalue with a date %s based on earliest %s and latest %s' % (averageYear, earliestyear, latestyear)
+        pywikibot.output(summary)
+        inceptionclaim.changeTarget(newdate, summary=summary)
 
 
 def main(*args):
