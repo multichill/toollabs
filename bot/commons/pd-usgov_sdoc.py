69cf3f78731131f520b4b28eed695d5076c7b633
multichill <maarten@mdammers.nl>
2024-08-15 23:15:33 +0200

    Also update existing statements

--- bot/commons/pd-usgov_sdoc.py
+++ bot/commons/pd-usgov_sdoc.py
@@ -28,11 +28,13 @@ class PDUSGovBot:
                                    'pd-usgov-atlas',
                                    'pd-usgov-blm',
                                    'pd-usgov-cia',
+                                   'pd-usgov-congress',
                                    'pd-usgov-fws',
                                    'pd-usgov-interior',
                                    'pd-usgov-military',
                                    'pd-usgov-military-air force',
                                    'pd-usgov-military-army',
+                                   'pd-usgov-military-marines',
                                    'pd-usgov-military-navy',
                                    'pd-usgov-nasa',
                                    'pd-usgov-noaa',
@@ -72,35 +74,47 @@ class PDUSGovBot:
         if not pd_found:
             return
 
-        summary = 'based on [[%s]]' % (pd_found, )
-
         media_info = file_page.data_item()
         data = media_info.get('data')
         claims = data.get('statements')
+
+        sdc_action = ''
 
         if 'P275' in claims:
             # Found a license, something is wrong
             return
         if 'P6216' in claims:
-            # TODO: Already has copyright status, expand later
-            return
-
-        new_claim = pywikibot.Claim(self.repo, 'P6216')  # copyright status (P6216)
-        new_claim.setTarget(self.pd_item)
+            # Already has a claim, try to update it
+            sdc_action = 'change'
+            summary = 'updating based on [[%s]]' % (pd_found, )
+            if len(claims.get('P6216')) != 1:
+                return
+            claim = claims.get('P6216')[0]
+            if claim.getTarget() != self.pd_item:
+                return
+            if claim.qualifiers:
+                return
+        else:
+            sdc_action = 'add'
+            summary = 'adding based on [[%s]]' % (pd_found, )
+            claim = pywikibot.Claim(self.repo, 'P6216')  # copyright status (P6216)
+            claim.setTarget(self.pd_item)
 
         qualifier = pywikibot.Claim(self.repo, 'P1001')  # applies to jurisdiction (P1001)
         qualifier.setTarget(self.usa_item)
         qualifier.isQualifier = True
-        new_claim.qualifiers[qualifier.getID()] = [qualifier]
+        claim.qualifiers[qualifier.getID()] = [qualifier]
 
         qualifier = pywikibot.Claim(self.repo, 'P459')  # determination method (P459)
         qualifier.setTarget(self.work_fed_usa_item)
         qualifier.isQualifier = True
-        new_claim.qualifiers[qualifier.getID()] = [qualifier]
+        claim.qualifiers[qualifier.getID()] = [qualifier]
 
-        print(summary)
-        media_info.addClaim(new_claim, summary=summary, tags='BotSDC')  # T372513 to not have the bot crash on the tags
-
+        pywikibot.output(summary)
+        if sdc_action == 'change':
+            self.site.save_claim(claim, summary=summary, tags='BotSDC')  # T372588 MediaInfo.changeClaim()
+        elif sdc_action == 'add':
+            media_info.addClaim(claim, summary=summary, tags='BotSDC')  # T372513 to not have the bot crash on the tags
 
 def main(*args):
     always_touch = False
